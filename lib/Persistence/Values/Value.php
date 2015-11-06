f9e2f56b0bc098c4404d05713117751b7124e4a8
Edi Modrić <edi.modric@gmail.com>
2015-11-06 16:19:32 +0100

    Make persistence value use our InvalidArgumentException

--- lib/Persistence/Values/Value.php
+++ lib/Persistence/Values/Value.php
@@ -2,12 +2,14 @@
 
 namespace Netgen\BlockManager\Persistence\Values;
 
-use InvalidArgumentException;
+use Netgen\BlockManager\Exceptions\InvalidArgumentException;
 
 abstract class Value
 {
     /**
      * Construct object optionally with a set of properties.
+     *
+     * @throws \Netgen\BlockManager\Exceptions\InvalidArgumentException If one of the properties does not exist in value object
      *
      * @param array $properties
      */
@@ -15,7 +17,11 @@ abstract class Value
     {
         foreach ($properties as $property => $value) {
             if (!property_exists($this, $property)) {
-                throw new InvalidArgumentException('Property "' . $property . '" does not exist in "' . get_class($this) . '" class.');
+                throw new InvalidArgumentException(
+                    'properties',
+                    $property,
+                    'Property "' . $property . '" does not exist in "' . get_class($this) . '" class.'
+                );
             }
 
             $this->$property = $value;
