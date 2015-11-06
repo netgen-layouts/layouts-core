889934ae33f8b716823e57ee9914a33bc57aee0e
Edi Modrić <edi.modric@gmail.com>
2015-11-06 11:58:09 +0100

    Throw our InvalidArgumentException in Value

--- lib/API/Values/Value.php
+++ lib/API/Values/Value.php
@@ -2,7 +2,7 @@
 
 namespace Netgen\BlockManager\API\Values;
 
-use InvalidArgumentException;
+use Netgen\BlockManager\Exceptions\InvalidArgumentException;
 
 abstract class Value
 {
@@ -12,13 +12,19 @@ abstract class Value
      * Read only properties values must be set using $properties as they are not writable anymore
      * after object has been created.
      *
+     * @throws \Netgen\BlockManager\Exceptions\InvalidArgumentException If one of the properties does not exist in value object
+     *
      * @param array $properties
      */
     public function __construct(array $properties = array())
     {
         foreach ($properties as $property => $value) {
             if (!property_exists($this, $property)) {
-                throw new InvalidArgumentException('Property "' . $property . '" does not exist in "' . get_class($this) . '" class.');
+                throw new InvalidArgumentException(
+                    'properties',
+                    $property,
+                    'Property "' . $property . '" does not exist in "' . get_class($this) . '" class.'
+                );
             }
 
             $this->$property = $value;
