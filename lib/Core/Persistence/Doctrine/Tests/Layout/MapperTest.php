c4d0fa1d9cce2f55e0e5c3fbbfa9bc117bf0c4c6
Edi Modrić <edi.modric@gmail.com>
2015-11-09 11:29:10 +0100

    Add doctrine mapper tests

---
+++ lib/Core/Persistence/Doctrine/Tests/Layout/MapperTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Netgen\BlockManager\Core\Persistence\Tests\Doctrine\Layout;
+
+use Netgen\BlockManager\Core\Persistence\Doctrine\Layout\Mapper;
+use Netgen\BlockManager\Persistence\Values\Page\Layout;
+
+class MapperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Netgen\BlockManager\Core\Persistence\Doctrine\Layout\Mapper::mapLayouts
+     */
+    public function testMapLayouts()
+    {
+        $data = array(
+            array(
+                'id' => 42,
+                'parent_id' => null,
+                'identifier' => '3_zones_a',
+                'created' => 123,
+                'modified' => 456,
+            ),
+            array(
+                'id' => 84,
+                'parent_id' => 48,
+                'identifier' => '3_zones_b',
+                'created' => 789,
+                'modified' => 111,
+            ),
+        );
+
+        $expectedData = array(
+            new Layout(
+                array(
+                    'id' => 42,
+                    'parentId' => null,
+                    'identifier' => '3_zones_a',
+                    'created' => 123,
+                    'modified' => 456,
+                )
+            ),
+            new Layout(
+                array(
+                    'id' => 84,
+                    'parentId' => 48,
+                    'identifier' => '3_zones_b',
+                    'created' => 789,
+                    'modified' => 111,
+                )
+            ),
+        );
+
+        $mapper = new Mapper();
+        self::assertEquals($expectedData, $mapper->mapLayouts($data));
+    }
+}
