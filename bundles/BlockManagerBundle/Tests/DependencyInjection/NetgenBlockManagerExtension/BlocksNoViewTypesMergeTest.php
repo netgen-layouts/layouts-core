43d78b1b8e19d5cfcd1ff071e6218569bc400f8e
Edi Modrić <edi.modric@gmail.com>
2015-11-13 19:56:55 +0100

    Add specific test for not being able to override block view types

---
+++ bundles/BlockManagerBundle/Tests/DependencyInjection/NetgenBlockManagerExtension/BlocksNoViewTypesMergeTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerBundle\Tests\DependencyInjection\NetgenBlockManagerExtension;
+
+use Netgen\Bundle\BlockManagerBundle\DependencyInjection\NetgenBlockManagerExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+
+class BlocksNoViewTypesMergeTest extends AbstractExtensionTestCase
+{
+    /**
+     * Return an array of container extensions that need to be registered for
+     * each test (usually just the container extension you are testing).
+     *
+     * @return \Symfony\Component\DependencyInjection\Extension\ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return array(
+            new NetgenBlockManagerExtension()
+        );
+    }
+
+    /**
+     * Returns minimal working configuration for the extension
+     */
+    protected function getMinimalConfiguration()
+    {
+        return array(
+            'blocks' => array(
+                'block' => array(
+                    'name' => 'block',
+                    'view_types' => array('small', 'large')
+                )
+            )
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\NetgenBlockManagerExtension::load
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getBlocksNodeDefinition
+     */
+    public function testNoViewTypesMerge()
+    {
+        $config = array(
+            'blocks' => array(
+                'block' => array(
+                    'name' => 'block',
+                    'view_types' => array('title', 'title_with_image')
+                )
+            )
+        );
+
+        $this->load($config);
+
+        $this->assertContainerBuilderHasParameter(
+            'netgen_block_manager.blocks',
+            array(
+                'block' => array(
+                    'name' => 'block',
+                    'view_types' => array('title', 'title_with_image')
+                )
+            )
+        );
+    }
+}
