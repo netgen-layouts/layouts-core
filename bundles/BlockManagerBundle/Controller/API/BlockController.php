9346957ae26a79674f75813fa8dd6dd728949bd2
Edi Modrić <edi.modric@gmail.com>
2015-11-19 13:07:49 +0100

    Implement creating the block through API

--- bundles/BlockManagerBundle/Controller/API/BlockController.php
+++ bundles/BlockManagerBundle/Controller/API/BlockController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Netgen\Bundle\BlockManagerBundle\Controller\Controller;
 use Netgen\BlockManager\Form\Data\UpdateBlockData;
 use Netgen\BlockManager\API\Values\Page\Block;
+use InvalidArgumentException;
 
 class BlockController extends Controller
 {
@@ -22,6 +23,51 @@ class BlockController extends Controller
         $blockView = $this->buildViewObject($block, array(), 'api');
 
         return $this->serializeObject($blockView);
+    }
+
+    /**
+     * Creates the block.
+     *
+     * @param string $definitionIdentifier
+     * @param string $viewType
+     * @param int|string $zoneId
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create($definitionIdentifier, $viewType, $zoneId)
+    {
+        $blockService = $this->get('netgen_block_manager.api.service.block');
+        $layoutService = $this->get('netgen_block_manager.api.service.layout');
+        $blockDefinition = $this->getBlockDefinition($definitionIdentifier);
+
+        $blockConfig = $this
+            ->get('netgen_block_manager.configuration')
+            ->getBlockConfig($definitionIdentifier);
+
+        if (!isset($blockConfig['view_types'][$viewType])) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Provided view type does not exist in "%s" block definition.',
+                    $definitionIdentifier
+                )
+            );
+        }
+
+        $blockCreateStruct = $blockService->newBlockCreateStruct($definitionIdentifier, $viewType);
+
+        foreach ($blockDefinition->getParameters() as $parameterIdentifier => $parameter) {
+            $blockCreateStruct->setParameter($parameterIdentifier, $parameter->getDefaultValue());
+        }
+
+        $createdBlock = $blockService->createBlock(
+            $blockCreateStruct,
+            $layoutService->loadZone($zoneId)
+        );
+
+        return $this->redirectToRoute(
+            'netgen_block_manager_api_v1_load_block',
+            array('blockId' => $createdBlock->getId())
+        );
     }
 
     /**
