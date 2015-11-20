cce6be7d6f5ef994879b080e2c752fb4fb322824
Edi Modrić <edi.modric@gmail.com>
2015-11-20 11:08:45 +0100

    Add PHPDoc on block controller

--- bundles/BlockManagerBundle/Controller/BlockController.php
+++ bundles/BlockManagerBundle/Controller/BlockController.php
@@ -6,6 +6,13 @@ use Netgen\BlockManager\API\Values\Page\Block;
 
 class BlockController extends Controller
 {
+    /**
+     * Renders the provided block
+     *
+     * @param \Netgen\BlockManager\API\Values\Page\Block $block
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function view(Block $block)
     {
         $blockView = $this->buildViewObject($block);
