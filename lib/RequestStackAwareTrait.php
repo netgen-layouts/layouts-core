72118ea0718fbb368235696a9e8dd470cc0b06e9
Edi Modrić <edi.modric@gmail.com>
2015-11-20 18:27:22 +0100

    Add RequestStackAwareTrait

---
+++ lib/RequestStackAwareTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Netgen\BlockManager;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+trait RequestStackAwareTrait
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\RequestStack
+     */
+    protected $requestStack;
+
+    /**
+     * Constructor.
+     *
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+     */
+    public function setRequestStack(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+}
