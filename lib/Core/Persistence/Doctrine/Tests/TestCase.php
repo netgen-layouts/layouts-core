00f3bc5f291bff84e7f379c28fb466a50851e2c3
Edi Modrić <edi.modric@gmail.com>
2015-11-22 14:33:50 +0100

    Refactor database tests to allow multiple test cases using the same trait

--- lib/Core/Persistence/Doctrine/Tests/TestCase.php
+++ lib/Core/Persistence/Doctrine/Tests/TestCase.php
@@ -6,96 +6,18 @@ use Netgen\BlockManager\Core\Persistence\Doctrine\Layout\Handler as LayoutHandle
 use Netgen\BlockManager\Core\Persistence\Doctrine\Layout\Mapper as LayoutMapper;
 use Netgen\BlockManager\Core\Persistence\Doctrine\Block\Handler as BlockHandler;
 use Netgen\BlockManager\Core\Persistence\Doctrine\Block\Mapper as BlockMapper;
-use Doctrine\DBAL\DriverManager;
-use Doctrine\DBAL\Types\Type;
+use Netgen\BlockManager\Tests\Traits\DoctrineDatabaseTrait;
 
 trait TestCase
 {
-    /**
-     * @var string
-     */
-    protected $databaseUri;
-
-    /**
-     * @var string
-     */
-    protected $databaseServer;
-
-    /**
-     * @var \Doctrine\DBAL\Connection
-     */
-    protected $databaseConnection;
+    use DoctrineDatabaseTrait;
 
     /**
      * Sets up the database connection.
      */
     protected function setUp()
     {
-        $databaseUri = getenv('DATABASE');
-        if (empty($databaseUri)) {
-            $databaseUri = 'sqlite://:memory:';
-        }
-
-        $this->databaseUri = $databaseUri;
-
-        preg_match('/^(?<db>.+):\/\//', $this->databaseUri, $matches);
-        $this->databaseServer = $matches['db'];
-
-        $this->createDatabaseConnection();
-        $this->createDatabaseSchema();
-        $this->insertDatabaseFixtures();
-    }
-
-    /**
-     * Creates the database connection.
-     */
-    protected function createDatabaseConnection()
-    {
-        $this->databaseConnection = DriverManager::getConnection(
-            array(
-                'url' => $this->databaseUri,
-            )
-        );
-    }
-
-    /**
-     * Creates the database schema.
-     */
-    protected function createDatabaseSchema()
-    {
-        $schema = file_get_contents(__DIR__ . '/_fixtures/schema/schema.' . $this->databaseServer . '.sql');
-        $sqlQueries = explode(';', $schema);
-
-        foreach ($sqlQueries as $sqlQuery) {
-            $sqlQuery = trim($sqlQuery);
-            if (!empty($sqlQuery)) {
-                $this->databaseConnection->query($sqlQuery);
-            }
-        }
-    }
-
-    /**
-     * Inserts database fixtures.
-     */
-    protected function insertDatabaseFixtures()
-    {
-        $data = require __DIR__ . '/_fixtures/data.php';
-
-        foreach ($data as $tableName => $tableData) {
-            if (!empty($tableData)) {
-                foreach ($tableData as $tableRow) {
-                    $this->databaseConnection
-                        ->createQueryBuilder()
-                        ->insert($tableName)
-                        ->values(array_fill_keys(array_keys($tableRow), '?'))
-                        ->setParameters(
-                            array_values($tableRow),
-                            array_fill_keys(array_keys($tableRow), Type::STRING)
-                        )
-                        ->execute();
-                }
-            }
-        }
+        $this->prepareDatabase(__DIR__ . '/_fixtures/schema', __DIR__ . '/_fixtures');
     }
 
     /**
