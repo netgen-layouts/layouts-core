a925c6bc67c26b1b42b30e41073e7ae567abb999
Edi Modrić <edi.modric@gmail.com>
2015-11-25 16:29:00 +0100

    Add layouts controller

---
+++ bundles/BlockManagerBundle/Controller/LayoutController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerBundle\Controller;
+
+class LayoutController extends Controller
+{
+    /**
+     * Creates the layout from specified layout identifier.
+     *
+     * @param string $identifier
+     * @param string $name
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create($identifier, $name)
+    {
+        $layoutService = $this->get('netgen_block_manager.api.service.layout');
+        $configuration = $this->get('netgen_block_manager.configuration');
+
+        $layoutConfig = $configuration->getLayoutConfig($identifier);
+
+        $layoutCreateStruct = $layoutService->newLayoutCreateStruct(
+            $identifier,
+            array_keys($layoutConfig['zones']),
+            $name
+        );
+
+        $layout = $layoutService->createLayout($layoutCreateStruct);
+
+        $layoutView = $this->buildViewObject($layout);
+
+        return $this->renderViewObject($layoutView);
+    }
+}
