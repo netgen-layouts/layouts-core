120766da6b311421511ea502665c88fd88bd6152
Edi Modrić <edi.modric@gmail.com>
2015-11-26 13:42:47 +0100

    Add event listener that injects inline edit form to API block view

---
+++ lib/EventListener/APIBlockViewListener.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Netgen\BlockManager\EventListener;
+
+use Netgen\BlockManager\API\Service\BlockService;
+use Netgen\BlockManager\Configuration\ConfigurationInterface;
+use Netgen\BlockManager\Event\View\CollectViewParametersEvent;
+use Netgen\BlockManager\Event\View\ViewEvents;
+use Netgen\BlockManager\View\BlockViewInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+
+class APIBlockViewListener implements EventSubscriberInterface
+{
+    /**
+     * @var \Netgen\BlockManager\API\Service\BlockService
+     */
+    protected $blockService;
+
+    /**
+     * @var \Netgen\BlockManager\Configuration\ConfigurationInterface
+     */
+    protected $configuration;
+
+    /**
+     * @var \Symfony\Component\Form\FormFactoryInterface
+     */
+    protected $formFactory;
+
+    /**
+     * Constructor.
+     *
+     * @param \Netgen\BlockManager\API\Service\BlockService $blockService
+     * @param \Netgen\BlockManager\Configuration\ConfigurationInterface $configuration
+     * @param \Symfony\Component\Form\FormFactoryInterface $formFactory
+     */
+    public function __construct(
+        BlockService $blockService,
+        ConfigurationInterface $configuration,
+        FormFactoryInterface $formFactory
+    ) {
+        $this->blockService = $blockService;
+        $this->configuration = $configuration;
+        $this->formFactory = $formFactory;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(ViewEvents::BUILD_VIEW => 'onBuildView');
+    }
+
+    /**
+     * Adds an inline edit form to API block views
+     *
+     * @param \Netgen\BlockManager\Event\View\CollectViewParametersEvent $event
+     */
+    public function onBuildView(CollectViewParametersEvent $event)
+    {
+        $view = $event->getView();
+        if (!$view instanceof BlockViewInterface || $view->getContext() !== 'api') {
+            return;
+        }
+
+        $block = $view->getBlock();
+        $blockConfig = $this->configuration->getBlockConfig($block->getDefinitionIdentifier());
+
+        if (!isset($blockConfig['forms']['inline'])) {
+            return;
+        }
+
+        $updateStruct = $this->blockService->newBlockUpdateStruct();
+        $updateStruct->setParameters($block->getParameters());
+        $updateStruct->viewType = $block->getViewType();
+        $updateStruct->name = $block->getName();
+
+        $form = $this->formFactory->create(
+            $blockConfig['forms']['inline'],
+            $updateStruct,
+            array('block' => $block)
+        );
+
+        $event->getParameterBag()->set('form', $form->createView());
+    }
+}
---
+++ lib/Tests/EventListener/APIBlockViewListenerTest.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace Netgen\BlockManager\Tests\EventListener;
+
+use Netgen\BlockManager\Core\Values\BlockUpdateStruct;
+use Netgen\BlockManager\Core\Values\Page\Block;
+use Netgen\BlockManager\Event\View\CollectViewParametersEvent;
+use Netgen\BlockManager\EventListener\APIBlockViewListener;
+use Netgen\BlockManager\View\BlockView;
+use Netgen\BlockManager\Event\View\ViewEvents;
+use Netgen\BlockManager\View\LayoutView;
+use Symfony\Component\Form\FormView;
+
+class APIBlockViewListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $blockServiceMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $configurationMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $formFactoryMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $formMock;
+
+    /**
+     * Sets up the tests
+     */
+    public function setUp()
+    {
+        $this->blockServiceMock = $this->getMock(
+            'Netgen\BlockManager\API\Service\BlockService'
+        );
+
+        $this->configurationMock = $this->getMock(
+            'Netgen\BlockManager\Configuration\ConfigurationInterface'
+        );
+
+        $this->formFactoryMock = $this->getMock(
+            'Symfony\Component\Form\FormFactoryInterface'
+        );
+
+        $this->formMock = $this->getMock(
+            'Symfony\Component\Form\FormInterface'
+        );
+
+        $this->formMock
+            ->expects($this->any())
+            ->method('createView')
+            ->will($this->returnValue(new FormView()));
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\EventListener\APIBlockViewListener::__construct
+     * @covers \Netgen\BlockManager\EventListener\APIBlockViewListener::getSubscribedEvents
+     */
+    public function testGetSubscribedEvents()
+    {
+        $listener = $this->getEventListener();
+        self::assertEquals(
+            array(ViewEvents::BUILD_VIEW => 'onBuildView'),
+            $listener->getSubscribedEvents()
+        );
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\EventListener\APIBlockViewListener::onBuildView
+     */
+    public function testOnBuildView()
+    {
+        $block = new Block(array('definitionIdentifier' => 'block'));
+        $blockView = new BlockView();
+        $blockView->setContext('api');
+        $blockView->setBlock($block);
+
+        $blockConfig = array('forms' => array('inline' => 'inline_form'));
+
+        $this->configurationMock
+            ->expects($this->once())
+            ->method('getBlockConfig')
+            ->with($this->equalTo('block'))
+            ->will($this->returnValue($blockConfig));
+
+        $blockUpdateStruct = new BlockUpdateStruct();
+
+        $this->blockServiceMock
+            ->expects($this->any())
+            ->method('newBlockUpdateStruct')
+            ->will($this->returnValue($blockUpdateStruct));
+
+        $this->formFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with(
+                $this->equalTo('inline_form'),
+                $this->equalTo($blockUpdateStruct),
+                $this->equalTo(array('block' => $block))
+            )
+            ->will($this->returnValue($this->formMock));
+
+        $event = new CollectViewParametersEvent($blockView, array());
+
+        $listener = $this->getEventListener();
+        $listener->onBuildView($event);
+
+        self::assertEquals(true, $event->getParameterBag()->has('form'));
+        self::assertInstanceOf('Symfony\Component\Form\FormView', $event->getParameterBag()->get('form'));
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\EventListener\APIBlockViewListener::onBuildView
+     */
+    public function testOnBuildViewWithNoBlockView()
+    {
+        $layoutView = new LayoutView();
+
+        $this->configurationMock
+            ->expects($this->never())
+            ->method('getBlockConfig');
+
+        $this->blockServiceMock
+            ->expects($this->never())
+            ->method('newBlockUpdateStruct');
+
+        $this->formFactoryMock
+            ->expects($this->never())
+            ->method('create');
+
+        $event = new CollectViewParametersEvent($layoutView, array());
+
+        $listener = $this->getEventListener();
+        $listener->onBuildView($event);
+
+        self::assertEquals(false, $event->getParameterBag()->has('form'));
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\EventListener\APIBlockViewListener::onBuildView
+     */
+    public function testOnBuildViewWithNoApiContext()
+    {
+        $blockView = new BlockView();
+
+        $this->configurationMock
+            ->expects($this->never())
+            ->method('getBlockConfig');
+
+        $this->blockServiceMock
+            ->expects($this->never())
+            ->method('newBlockUpdateStruct');
+
+        $this->formFactoryMock
+            ->expects($this->never())
+            ->method('create');
+
+        $event = new CollectViewParametersEvent($blockView, array());
+
+        $listener = $this->getEventListener();
+        $listener->onBuildView($event);
+
+        self::assertEquals(false, $event->getParameterBag()->has('form'));
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\EventListener\APIBlockViewListener::onBuildView
+     */
+    public function testOnBuildViewWithNoInlineForm()
+    {
+        $block = new Block(array('definitionIdentifier' => 'block'));
+        $blockView = new BlockView();
+        $blockView->setContext('api');
+        $blockView->setBlock($block);
+
+        $this->configurationMock
+            ->expects($this->once())
+            ->method('getBlockConfig')
+            ->with($this->equalTo('block'))
+            ->will($this->returnValue(array()));
+
+        $this->blockServiceMock
+            ->expects($this->never())
+            ->method('newBlockUpdateStruct');
+
+        $this->formFactoryMock
+            ->expects($this->never())
+            ->method('create');
+
+        $event = new CollectViewParametersEvent($blockView, array());
+
+        $listener = $this->getEventListener();
+        $listener->onBuildView($event);
+
+        self::assertEquals(false, $event->getParameterBag()->has('form'));
+    }
+
+    /**
+     * Returns the listener under test
+     *
+     * @return \Netgen\BlockManager\EventListener\APIBlockViewListener
+     */
+    protected function getEventListener()
+    {
+        return new APIBlockViewListener(
+            $this->blockServiceMock,
+            $this->configurationMock,
+            $this->formFactoryMock
+        );
+    }
+}
