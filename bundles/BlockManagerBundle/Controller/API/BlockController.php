9388fbf103183b8f9a9231265a1a5115df28edae
Edi Modrić <edi.modric@gmail.com>
2016-02-09 10:21:12 +0100

    Output block types and block type groups as regular arrays

--- bundles/BlockManagerBundle/Controller/API/BlockController.php
+++ bundles/BlockManagerBundle/Controller/API/BlockController.php
@@ -64,9 +64,26 @@ class BlockController extends Controller
         $serializer = $this->get('serializer');
         $configuration = $this->get('netgen_block_manager.configuration');
 
+        $configBlockTypeGroups = $configuration->getParameter('block_type_groups');
+        $configBlockTypes = $configuration->getParameter('block_types');
+
+        $blockTypeGroups = array();
+        foreach ($configBlockTypeGroups as $identifier => $blockTypeGroup) {
+            $blockTypeGroups[] = array(
+                'identifier' => $identifier
+            ) + $blockTypeGroup;
+        }
+
+        $blockTypes = array();
+        foreach ($configBlockTypes as $identifier => $blockType) {
+            $blockTypes[] = array(
+                'identifier' => $identifier
+            ) + $blockType;
+        }
+
         $data = array(
-            'block_type_groups' => $configuration->getParameter('block_type_groups'),
-            'block_types' => $configuration->getParameter('block_types')
+            'block_type_groups' => $blockTypeGroups,
+            'block_types' => $blockTypes
         );
 
         $response = new JsonResponse();
