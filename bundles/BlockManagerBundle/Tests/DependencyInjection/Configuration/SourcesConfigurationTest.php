aec3eacfa1a5ec08e10515af3a095a3673c7cf39
Edi Modrić <edi.modric@gmail.com>
2016-05-10 16:00:59 +0200

    Add tests for sources configuration

---
+++ bundles/BlockManagerBundle/Tests/DependencyInjection/Configuration/SourcesConfigurationTest.php
@@ -0,0 +1,499 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerBundle\Tests\DependencyInjection\Configuration;
+
+use Netgen\Bundle\BlockManagerBundle\DependencyInjection\NetgenBlockManagerExtension;
+use Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration;
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+
+class SourcesConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    /**
+     * Return the instance of ConfigurationInterface that should be used by the
+     * Configuration-specific assertions in this test-case.
+     *
+     * @return \Symfony\Component\Config\Definition\ConfigurationInterface
+     */
+    protected function getConfiguration()
+    {
+        $extension = new NetgenBlockManagerExtension();
+
+        return new Configuration($extension->getAlias());
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     */
+    public function testSourceSettings()
+    {
+        $config = array(
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type',
+                                'default_parameters' => array(
+                                    'param' => 'value'
+                                )
+                            )
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $expectedConfig = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => 'type',
+                            'default_parameters' => array(
+                                'param' => 'value'
+                            )
+                        )
+                    )
+                ),
+            ),
+        );
+
+        $this->assertProcessedConfigurationEquals(
+            $config,
+            $expectedConfig,
+            'sources'
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     */
+    public function testSourceSettingsWithDefaultSource()
+    {
+        $config = array(
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'query_type' => 'type',
+                        'default_parameters' => array(
+                            'param' => 'value'
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $expectedConfig = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type',
+                                'default_parameters' => array(
+                                    'param' => 'value'
+                                )
+                            )
+                        )
+                ),
+            ),
+        );
+
+        $this->assertProcessedConfigurationEquals(
+            $config,
+            $expectedConfig,
+            'sources'
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     */
+    public function testSourceSettingsWithNoQueriesMerge()
+    {
+        $config = array(
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type1',
+                                'default_parameters' => array(
+                                    'param1' => 'value1',
+                                    'param2' => 'value2',
+                                )
+                            )
+                        )
+                    ),
+                ),
+            ),
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type2',
+                                'default_parameters' => array(
+                                    'param3' => 'value3'
+                                )
+                            )
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $expectedConfig = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => 'type2',
+                            'default_parameters' => array(
+                                'param3' => 'value3'
+                            )
+                        )
+                    )
+                ),
+            ),
+        );
+
+        $this->assertProcessedConfigurationEquals(
+            $config,
+            $expectedConfig,
+            'sources'
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     */
+    public function testSourceSettingsNoParametersMerge()
+    {
+        $config = array(
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type',
+                                'default_parameters' => array(
+                                    'param' => 'value',
+                                    'param2' => 'value2'
+                                )
+                            )
+                        )
+                    ),
+                ),
+            ),
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type',
+                                'default_parameters' => array(
+                                    'param3' => 'value3'
+                                )
+                            )
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $expectedConfig = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => 'type',
+                            'default_parameters' => array(
+                                'param3' => 'value3'
+                            )
+                        )
+                    )
+                ),
+            ),
+        );
+
+        $this->assertProcessedConfigurationEquals(
+            $config,
+            $expectedConfig,
+            'sources'
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     */
+    public function testSourceSettingsWithNoDefaultParameters()
+    {
+        $config = array(
+            array(
+                'sources' => array(
+                    'dynamic' => array(
+                        'name' => 'Dynamic',
+                        'queries' => array(
+                            'default' => array(
+                                'query_type' => 'type',
+                            )
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $expectedConfig = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => 'type',
+                            'default_parameters' => array(),
+                        )
+                    )
+                ),
+            ),
+        );
+
+        $this->assertProcessedConfigurationEquals(
+            $config,
+            $expectedConfig,
+            'sources'
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::__construct
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getAvailableNodeDefinitions
+     */
+    public function testSourceSettingsWithEmptyDefaultParameters()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => 'type',
+                            'default_parameters' => array()
+                        )
+                    )
+                )
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithEmptySources()
+    {
+        $config = array('sources' => array());
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithInvalidSources()
+    {
+        $config = array('sources' => 'sources');
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithNoName()
+    {
+        $config = array('sources' => array('dynamic' => array()));
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithEmptyName()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => '',
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithInvalidName()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => array(),
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithNoQueries()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithEmptyQueries()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(),
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithInvalidQueries()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => 'queries',
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithNoQueryType()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array()
+                    ),
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithEmptyQueryType()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => ''
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+
+    /**
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getConfigTreeBuilder
+     * @covers \Netgen\Bundle\BlockManagerBundle\DependencyInjection\Configuration::getSourcesNodeDefinition
+     */
+    public function testSourceSettingsWithInvalidQueryType()
+    {
+        $config = array(
+            'sources' => array(
+                'dynamic' => array(
+                    'name' => 'Dynamic',
+                    'queries' => array(
+                        'default' => array(
+                            'query_type' => array()
+                        )
+                    ),
+                ),
+            ),
+        );
+
+        $this->assertConfigurationIsInvalid(array($config));
+    }
+}
