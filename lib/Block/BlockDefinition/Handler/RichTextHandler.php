3146841124b381f734c70cd717050ce76134b20a
Edi Modrić <edi.modric@gmail.com>
2016-06-14 02:47:52 +0200

    Add rich text block type and definition

---
+++ lib/Block/BlockDefinition/Handler/RichTextHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Netgen\BlockManager\Block\BlockDefinition\Handler;
+
+use Netgen\BlockManager\Block\BlockDefinition\BlockDefinitionHandlerInterface;
+use Netgen\BlockManager\Block\BlockDefinition\BlockDefinitionHandler;
+use Netgen\BlockManager\Parameters\Parameter;
+
+class RichTextHandler extends BlockDefinitionHandler implements BlockDefinitionHandlerInterface
+{
+    /**
+     * Returns the array specifying block parameters.
+     *
+     * @return \Netgen\BlockManager\Parameters\ParameterInterface[]
+     */
+    public function getParameters()
+    {
+        return array(
+            'content' => new Parameter\Html(),
+        ) + parent::getParameters();
+    }
+}
---
+++ lib/Parameters/FormMapper/ParameterHandler/Html.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Netgen\BlockManager\Parameters\FormMapper\ParameterHandler;
+
+use Netgen\BlockManager\Parameters\FormMapper\ParameterHandler;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class Html extends ParameterHandler
+{
+    /**
+     * Returns the form type for the parameter.
+     *
+     * @return string
+     */
+    protected function getFormType()
+    {
+        return TextareaType::class;
+    }
+}
