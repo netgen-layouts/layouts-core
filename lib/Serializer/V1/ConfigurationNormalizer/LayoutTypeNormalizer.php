5e835b04de2e4f10e07c59d48484f1ef352f9c70
Edi Modrić <edi.modric@gmail.com>
2016-06-16 16:19:30 +0200

    Add layout type normalizer

---
+++ lib/Serializer/V1/ConfigurationNormalizer/LayoutTypeNormalizer.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Netgen\BlockManager\Serializer\V1\ConfigurationNormalizer;
+
+use Netgen\BlockManager\Configuration\LayoutType\LayoutType;
+use Netgen\BlockManager\Serializer\Values\VersionedValue;
+use Netgen\BlockManager\Serializer\Version;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class LayoutTypeNormalizer implements NormalizerInterface
+{
+    /**
+     * Normalizes an object into a set of arrays/scalars.
+     *
+     * @param \Netgen\BlockManager\Serializer\Values\VersionedValue $object
+     * @param string $format
+     * @param array $context
+     *
+     * @return array
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        /** @var \Netgen\BlockManager\Configuration\LayoutType\LayoutType $layoutType */
+        $layoutType = $object->getValue();
+
+        return array(
+            'identifier' => $layoutType->getIdentifier(),
+            'name' => $layoutType->getName(),
+            'zones' => $this->getZones($layoutType),
+        );
+    }
+
+    /**
+     * Returns the array with layout type zones.
+     *
+     * @param \Netgen\BlockManager\Configuration\LayoutType\LayoutType $layoutType
+     *
+     * @return array
+     */
+    protected function getZones(LayoutType $layoutType)
+    {
+        $zones = array();
+
+        foreach ($layoutType->getZones() as $zoneIdentifier => $zone) {
+            $zones[] = array(
+                'identifier' => $zoneIdentifier,
+                'allowed_block_definitions' => !empty($zone->getAllowedBlockDefinitions()) ?
+                    $zone->getAllowedBlockDefinitions() :
+                    true,
+            );
+        }
+
+        return $zones;
+    }
+
+    /**
+     * Checks whether the given class is supported for normalization by this normalizer.
+     *
+     * @param mixed $data
+     * @param string $format
+     *
+     * @return bool
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        if (!$data instanceof VersionedValue) {
+            return false;
+        }
+
+        return $data->getValue() instanceof LayoutType && $data->getVersion() === Version::API_V1;
+    }
+}
