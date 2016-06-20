6d2c9643db738b17b2e155cef796e7289f17be39
Edi Modrić <edi.modric@gmail.com>
2016-06-20 17:38:25 +0200

    Add min and max to number form

--- lib/Parameters/FormMapper/ParameterHandler/Number.php
+++ lib/Parameters/FormMapper/ParameterHandler/Number.php
@@ -3,6 +3,7 @@
 namespace Netgen\BlockManager\Parameters\FormMapper\ParameterHandler;
 
 use Netgen\BlockManager\Parameters\FormMapper\ParameterHandler;
+use Netgen\BlockManager\Parameters\ParameterInterface;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class Number extends ParameterHandler
@@ -15,5 +16,32 @@ class Number extends ParameterHandler
     protected function getFormType()
     {
         return NumberType::class;
+    }
+
+    /**
+     * Converts parameter options to Symfony form options.
+     *
+     * @param \Netgen\BlockManager\Parameters\ParameterInterface $parameter
+     *
+     * @return array
+     */
+    protected function convertOptions(ParameterInterface $parameter)
+    {
+        $parameterOptions = $parameter->getOptions();
+
+        $attributes = array();
+        if ($parameterOptions['min'] !== null || $parameterOptions['max'] !== null) {
+            if ($parameterOptions['min'] !== null) {
+                $attributes['min'] = $parameterOptions['min'];
+            }
+
+            if ($parameterOptions['max'] !== null) {
+                $attributes['max'] = $parameterOptions['max'];
+            }
+        }
+
+        return array(
+            'attr' => $attributes,
+        );
     }
 }
