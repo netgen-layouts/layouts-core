478dbfaa1eac639a6437d4564fd1b8a21056ce6f
Edi Modrić <edi.modric@gmail.com>
2016-06-24 12:36:29 +0200

    Enable loading app UI with different environments

--- bundles/BlockManagerAdminUIBundle/Controller/App/IndexController.php
+++ bundles/BlockManagerAdminUIBundle/Controller/App/IndexController.php
@@ -3,17 +3,27 @@
 namespace Netgen\Bundle\BlockManagerAdminUIBundle\Controller\App;
 
 use Netgen\Bundle\BlockManagerBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller
 {
     /**
      * Displays the Block Manager app index page.
      *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $appEnvironment = $request->attributes->get('_ngbm_environment', '');
+
         return $this->render(
+            !empty($appEnvironment) ?
+                sprintf(
+                    'NetgenBlockManagerAdminUIBundle:app/index:index_%s.html.twig',
+                    $appEnvironment
+                ) :
             'NetgenBlockManagerAdminUIBundle:app/index:index.html.twig'
         );
     }
