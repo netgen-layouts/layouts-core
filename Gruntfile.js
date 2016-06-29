4b338789619decd17398e834eb06e5ee7ec410db
Edi Modrić <edi.modric@gmail.com>
2016-06-29 15:21:42 +0200

    Use grunt.log.writeln

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
   grunt.registerTask('default', function(){
-    console.log('');
+    grunt.log.writeln('');
     grunt.log.error('*************************************************');
     grunt.log.error('Grunt needs to be started from one of the bundles');
     grunt.log.error('*************************************************');
