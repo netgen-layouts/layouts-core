131cbba90f90f15678e76110574763babeeb86ff
Edi Modrić <edi.modric@gmail.com>
2016-07-08 10:27:40 +0200

    Add Version class

---
+++ lib/Version.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Netgen\BlockManager;
+
+abstract class Version
+{
+    const VERSION = '1.0.0-DEV';
+    const VERSION_ID = 10000;
+    const MAJOR_VERSION = 1;
+    const MINOR_VERSION = 0;
+    const RELEASE_VERSION = 0;
+    const EXTRA_VERSION = 'DEV';
+}
