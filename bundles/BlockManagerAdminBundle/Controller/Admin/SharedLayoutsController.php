75f65cfcbaf943f1a568a7b63bf56edf3e167316
Edi Modrić <edi.modric@gmail.com>
2016-07-13 15:45:10 +0200

    Add initial version of shared layouts controller

---
+++ bundles/BlockManagerAdminBundle/Controller/Admin/SharedLayoutsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerAdminBundle\Controller\Admin;
+
+use Netgen\BlockManager\API\Service\LayoutService;
+use Netgen\Bundle\BlockManagerBundle\Controller\Controller;
+
+class SharedLayoutsController extends Controller
+{
+    /**
+     * @var \Netgen\BlockManager\API\Service\LayoutService
+     */
+    protected $layoutService;
+
+    /**
+     * Constructor.
+     *
+     * @param \Netgen\BlockManager\API\Service\LayoutService $layoutService
+     */
+    public function __construct(LayoutService $layoutService)
+    {
+        $this->layoutService = $layoutService;
+    }
+
+    /**
+     * Displays the index page of shared layouts admin interface.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        return $this->render(
+            'NetgenBlockManagerAdminBundle:admin/shared_layouts:index.html.twig'
+        );
+    }
+}
