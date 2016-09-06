253f63e82ae25c43f96c057bdd0dada98338f7a0
Edi Modrić <edi.modric@gmail.com>
2016-09-06 17:41:34 +0200

    Implement markdown block, disabled for now

---
+++ lib/Block/BlockDefinition/Handler/MarkdownHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Netgen\BlockManager\Block\BlockDefinition\Handler;
+
+use Netgen\BlockManager\API\Values\Page\Block;
+use Netgen\BlockManager\Parameters\Parameter\Text;
+use Netgen\BlockManager\Block\BlockDefinition\BlockDefinitionHandler;
+use Michelf\MarkdownInterface;
+
+class MarkdownHandler extends BlockDefinitionHandler
+{
+    /**
+     * @var \Michelf\MarkdownInterface
+     */
+    protected $markdownParser;
+
+    /**
+     * Constructor.
+     *
+     * @param \Michelf\MarkdownInterface $markdownParser
+     */
+    public function __construct(MarkdownInterface $markdownParser)
+    {
+        $this->markdownParser = $markdownParser;
+    }
+
+    /**
+     * Returns the array specifying block parameters.
+     *
+     * @return \Netgen\BlockManager\Parameters\ParameterInterface[]
+     */
+    public function getParameters()
+    {
+        return array(
+            'content' => new Text(),
+        ) + $this->getCommonParameters();
+    }
+
+    /**
+     * Returns the array of dynamic parameters provided by this block definition.
+     *
+     * @param \Netgen\BlockManager\API\Values\Page\Block $block
+     *
+     * @return array
+     */
+    public function getDynamicParameters(Block $block)
+    {
+        $rawContent = $block->getParameter('content');
+
+        return array(
+            'html' => function () use ($rawContent) {
+                return $this->markdownParser->transform($rawContent);
+            },
+        );
+    }
+}
