1a88ce4f03a658299cc8b53b3e001e7fb2b433f3
Edi Modrić <edi.modric@gmail.com>
2016-09-09 15:06:25 +0200

    Add rule count layout view listener

---
+++ bundles/BlockManagerAdminBundle/EventListener/LayoutView/RuleCountListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerAdminBundle\EventListener\LayoutView;
+
+use Netgen\BlockManager\API\Service\LayoutResolverService;
+use Netgen\BlockManager\View\View\LayoutViewInterface;
+use Netgen\BlockManager\View\ViewInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Netgen\BlockManager\Event\View\CollectViewParametersEvent;
+use Netgen\BlockManager\Event\View\ViewEvents;
+
+class RuleCountListener implements EventSubscriberInterface
+{
+    /**
+     * @var \Netgen\BlockManager\API\Service\LayoutResolverService
+     */
+    protected $layoutResolverService;
+
+    /**
+     * Constructor.
+     *
+     * @param \Netgen\BlockManager\API\Service\LayoutResolverService $layoutResolverService
+     */
+    public function __construct(LayoutResolverService $layoutResolverService)
+    {
+        $this->layoutResolverService = $layoutResolverService;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(ViewEvents::BUILD_VIEW => 'onBuildView');
+    }
+
+    /**
+     * Includes results built from all block collections, if specified so.
+     *
+     * @param \Netgen\BlockManager\Event\View\CollectViewParametersEvent $event
+     */
+    public function onBuildView(CollectViewParametersEvent $event)
+    {
+        $view = $event->getView();
+        if (!$view instanceof LayoutViewInterface) {
+            return;
+        }
+
+        if ($view->getContext() !== ViewInterface::CONTEXT_ADMIN) {
+            return;
+        }
+
+        $ruleCount = $this->layoutResolverService->getRuleCount(
+            $view->getLayout()
+        );
+
+        $event->getParameterBag()->add(array('rule_count' => $ruleCount));
+    }
+}
