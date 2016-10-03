06d3718fc25efa67211dee0b61119135eee71f5b
Edi Modrić <edi.modric@gmail.com>
2016-10-03 23:44:31 +0200

    Remove unused method

--- bundles/BlockManagerBundle/Controller/Controller.php
+++ bundles/BlockManagerBundle/Controller/Controller.php
@@ -35,20 +35,6 @@ abstract class Controller extends BaseController
     }
 
     /**
-     * Returns the specified block definition from the registry.
-     *
-     * @param string $definitionIdentifier
-     *
-     * @return \Netgen\BlockManager\Block\BlockDefinitionInterface
-     */
-    protected function getBlockDefinition($definitionIdentifier)
-    {
-        $blockDefinitionRegistry = $this->get('netgen_block_manager.block.registry.block_definition');
-
-        return $blockDefinitionRegistry->getBlockDefinition($definitionIdentifier);
-    }
-
-    /**
      * Returns the specified query type from the registry.
      *
      * @param string $identifier
