9f09d7a45331c32001c5404997a7700a69ac3833
Edi Modrić <edi.modric@gmail.com>
2016-11-02 15:09:45 +0100

    Add admin auth exception listener

---
+++ bundles/BlockManagerAdminBundle/EventListener/AdminAuthenticationExceptionListener.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerAdminBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+class AdminAuthenticationExceptionListener implements EventSubscriberInterface
+{
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        // Priority needs to be higher than built in exception listener
+        return array(KernelEvents::EXCEPTION => array('onException', 20));
+    }
+
+    /**
+     * Converts exceptions to Symfony HTTP exceptions.
+     *
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event
+     */
+    public function onException(GetResponseForExceptionEvent $event)
+    {
+        $attributes = $event->getRequest()->attributes;
+        if ($attributes->get(SetIsAdminRequestListener::ADMIN_FLAG_NAME) !== true) {
+            return;
+        }
+
+        if (!$event->getRequest()->isXmlHttpRequest()) {
+            return;
+        }
+
+        $exception = $event->getException();
+        if (!$exception instanceof AuthenticationException && !$exception instanceof AccessDeniedException) {
+            return;
+        }
+
+        $event->setException(
+            new AccessDeniedHttpException($exception->getMessage())
+        );
+
+        $event->stopPropagation();
+    }
+}
