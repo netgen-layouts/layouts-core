421896953cf85e363a6fac85a7552489430c20e0
Edi Modrić <edi.modric@gmail.com>
2016-12-05 13:16:22 +0100

    CS on Grunt and Bower files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,9 @@
 module.exports = function (grunt) {
-  grunt.registerTask('default', function(){
-    grunt.log.writeln('');
-    grunt.log.error('*************************************************');
-    grunt.log.error('Grunt needs to be started from one of the bundles');
-    grunt.log.error('*************************************************');
-  });
+    grunt.registerTask('default', function() {
+        grunt.log.writeln();
+
+        grunt.log.error('*****************************************************');
+        grunt.log.error('* Grunt needs to be started from one of the bundles *');
+        grunt.log.error('*****************************************************');
+    });
 };
--- bundles/BlockManagerAdminBundle/Gruntfile.js
+++ bundles/BlockManagerAdminBundle/Gruntfile.js
@@ -1,100 +1,96 @@
 'use strict';
 
 module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+    // Automatically load required grunt tasks
+    require('jit-grunt')(grunt, {
+        lockfile: 'grunt-lock'
+    });
 
-  // Automatically load required grunt tasks
-  require('jit-grunt')(grunt,{
-    lockfile: 'grunt-lock'
-  });
+    // Configurable paths
+    var config = {
+        resources_dir: 'Resources/',
+        public_dir: 'Resources/public/'
+    };
 
-  // Configurable paths
-  var config = {
-    resources_dir: 'Resources/',
-    public_dir: 'Resources/public/'
-  };
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        // Project settings
+        config: config,
 
-  // Define the configuration for all the tasks
-  grunt.initConfig({
+        //Prevent multiple grunt instances
+        lockfile: {
+            grunt: {
+                path: 'grunt.lock'
+            }
+        },
 
-    // Project settings
-    config: config,
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            gruntfile: {
+                files: ['Gruntfile.js'],
+                options: {
+                    reload: true
+                }
+            },
+            sass: {
+                files: ['<%= config.resources_dir %>/sass/{,*/}*.{scss,sass}'],
+                tasks: ['sass', 'postcss']
+            }
+        },
 
-    //Prevent multiple grunt instances
-    lockfile: {
-      grunt: {
-        path: 'grunt.lock'
-      }
-    },
+        // Compiles Sass to CSS and generates necessary files if requested
+        sass: {
+            options: {
+                sourceMap: true,
+                sourceMapEmbed: true,
+                sourceMapContents: true,
+                includePaths: ['.']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.resources_dir %>/sass',
+                    src: ['*.{scss,sass}'],
+                    dest: '.tmp/css',
+                    ext: '.css'
+                }]
+            }
+        },
 
-    // Watches files for changes and runs tasks based on the changed files
-    watch: {
-      gruntfile: {
-        files: ['Gruntfile.js'],
-         options: {
-           reload: true
-         }
-      },
-      sass: {
-        files: ['<%= config.resources_dir %>/sass/{,*/}*.{scss,sass}'],
-        tasks: ['sass', 'postcss']
-      }
-    },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    // Add vendor prefixed styles
+                    require('autoprefixer-core')({
+                        browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1']
+                    })
+                ]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/css/',
+                    src: '{,*/}*.css',
+                    dest: '<%= config.public_dir %>/css'
+                }]
+            }
+        }
+    });
 
-    // Compiles Sass to CSS and generates necessary files if requested
-    sass: {
-      options: {
-        sourceMap: true,
-        sourceMapEmbed: true,
-        sourceMapContents: true,
-        includePaths: ['.']
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.resources_dir %>/sass',
-          src: ['*.{scss,sass}'],
-          dest: '.tmp/css',
-          ext: '.css'
-        }]
-      }
-    },
+    grunt.registerTask('serve', 'Start the server and preview your app', function () {
+        grunt.task.run([
+            'lockfile',
+            'sass:dist',
+            'postcss',
+            'watch'
+        ]);
+    });
 
-    postcss: {
-      options: {
-        map: true,
-        processors: [
-          // Add vendor prefixed styles
-          require('autoprefixer-core')({
-            browsers: ['> 1%', 'last 3 versions', 'Firefox ESR', 'Opera 12.1']
-          })
-        ]
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/css/',
-          src: '{,*/}*.css',
-          dest: '<%= config.public_dir %>/css'
-        }]
-      }
-    }
-
-  });
-
-  grunt.registerTask('serve', 'start the server and preview your app', function () {
-
-    grunt.task.run([
-      'lockfile',
-      'sass:dist',
-      'postcss',
-      'watch'
+    grunt.registerTask('default', [
+        'serve'
     ]);
-  });
-
-  grunt.registerTask('default', [
-    'serve'
-  ]);
 };
