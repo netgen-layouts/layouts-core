803fea818e58e94d491fa61dc86b6ef8a58b6131
Edi Modrić <edi.modric@gmail.com>
2016-12-05 15:39:54 +0100

    Do not track rendered block views in API and admin contexts

--- bundles/BlockManagerDebugBundle/DataCollector/EventListener/BlockViewListener.php
+++ bundles/BlockManagerDebugBundle/DataCollector/EventListener/BlockViewListener.php
@@ -5,11 +5,17 @@ namespace Netgen\Bundle\BlockManagerDebugBundle\DataCollector\EventListener;
 use Netgen\BlockManager\Event\BlockManagerEvents;
 use Netgen\BlockManager\Event\CollectViewParametersEvent;
 use Netgen\BlockManager\View\View\BlockViewInterface;
+use Netgen\BlockManager\View\ViewInterface;
 use Netgen\Bundle\BlockManagerDebugBundle\DataCollector\BlockManagerDataCollector;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class BlockViewListener implements EventSubscriberInterface
 {
+    const BLACKLISTED_CONTEXTS = array(
+        ViewInterface::CONTEXT_API,
+        ViewInterface::CONTEXT_ADMIN,
+    );
+
     /**
      * @var \Netgen\Bundle\BlockManagerDebugBundle\DataCollector\BlockManagerDataCollector
      */
@@ -45,6 +51,10 @@ class BlockViewListener implements EventSubscriberInterface
         $view = $event->getView();
 
         if (!$event->getView() instanceof BlockViewInterface) {
+            return;
+        }
+
+        if (in_array($view->getContext(), self::BLACKLISTED_CONTEXTS, true)) {
             return;
         }
 
