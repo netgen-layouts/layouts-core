bae76bcbc3db2770db2d01d62945ec4786c5cb5c
Edi Modrić <edi.modric@gmail.com>
2016-12-29 09:26:37 +0100

    Test creating a container block definition

--- tests/lib/Block/BlockDefinitionFactoryTest.php
+++ tests/lib/Block/BlockDefinitionFactoryTest.php
@@ -6,6 +6,7 @@ use Netgen\BlockManager\Block\BlockDefinition\BlockDefinitionHandlerInterface;
 use Netgen\BlockManager\Block\BlockDefinition\Configuration\Configuration;
 use Netgen\BlockManager\Block\BlockDefinitionFactory;
 use Netgen\BlockManager\Block\BlockDefinitionInterface;
+use Netgen\BlockManager\Block\PlaceholderDefinitionInterface;
 use Netgen\BlockManager\Parameters\ParameterBuilderInterface;
 use PHPUnit\Framework\TestCase;
 
@@ -53,5 +54,41 @@ class BlockDefinitionFactoryTest extends TestCase
         $this->assertInstanceOf(BlockDefinitionInterface::class, $blockDefinition);
         $this->assertEquals('definition', $blockDefinition->getIdentifier());
         $this->assertEquals($this->configMock, $blockDefinition->getConfig());
+        $this->assertFalse($blockDefinition->isContainer());
+
+        $this->assertCount(0, $blockDefinition->getPlaceholders());
+    }
+
+    /**
+     * @covers \Netgen\BlockManager\Block\BlockDefinitionFactory::buildBlockDefinition
+     */
+    public function testBuildContainerBlockDefinition()
+    {
+        $this->handlerMock
+            ->expects($this->any())
+            ->method('getPlaceholderIdentifiers')
+            ->will($this->returnValue(array('left', 'right')));
+
+        $this->handlerMock
+            ->expects($this->any())
+            ->method('isContainer')
+            ->will($this->returnValue(true));
+
+        $blockDefinition = BlockDefinitionFactory::buildBlockDefinition(
+            'definition',
+            $this->handlerMock,
+            $this->configMock,
+            $this->parameterBuilderMock
+        );
+
+        $this->assertInstanceOf(BlockDefinitionInterface::class, $blockDefinition);
+        $this->assertEquals('definition', $blockDefinition->getIdentifier());
+        $this->assertEquals($this->configMock, $blockDefinition->getConfig());
+        $this->assertTrue($blockDefinition->isContainer());
+
+        $this->assertCount(2, $blockDefinition->getPlaceholders());
+        foreach ($blockDefinition->getPlaceholders() as $placeholder) {
+            $this->assertInstanceOf(PlaceholderDefinitionInterface::class, $placeholder);
+        }
     }
 }
