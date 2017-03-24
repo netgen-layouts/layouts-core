673175a44487b402baecafbec3ae2d6daff7495d
Edi Modrić <edi.modric@gmail.com>
2017-03-24 14:23:00 +0100

    Add interface for editing block config

--- bundles/BlockManagerAdminBundle/Controller/App/BlockController.php
+++ bundles/BlockManagerAdminBundle/Controller/App/BlockController.php
@@ -5,7 +5,9 @@ namespace Netgen\Bundle\BlockManagerAdminBundle\Controller\App;
 use Netgen\BlockManager\API\Service\BlockService;
 use Netgen\BlockManager\API\Service\CollectionService;
 use Netgen\BlockManager\API\Values\Block\Block;
+use Netgen\BlockManager\API\Values\Config\ConfigStruct;
 use Netgen\BlockManager\Collection\Registry\QueryTypeRegistryInterface;
+use Netgen\BlockManager\Config\Form\EditType as ConfigEditType;
 use Netgen\BlockManager\View\ViewInterface;
 use Netgen\Bundle\BlockManagerBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -98,6 +100,65 @@ class BlockController extends Controller
                     array(
                         'blockId' => $block->getId(),
                         'formName' => $formName,
+                    )
+                ),
+            )
+        );
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted()) {
+            return $this->buildView($form, ViewInterface::CONTEXT_API);
+        }
+
+        if ($form->isValid()) {
+            $this->blockService->updateBlock($block, $form->getData());
+
+            return new Response(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return $this->buildView(
+            $form,
+            ViewInterface::CONTEXT_API,
+            array(),
+            new Response(null, Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+
+    /**
+     * Displays and processes block config edit form.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Netgen\BlockManager\API\Values\Block\Block $block
+     * @param string $identifier
+     *
+     * @return \Netgen\BlockManager\View\ViewInterface|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editConfigForm(Request $request, Block $block, $identifier = null)
+    {
+        $updateStruct = $this->blockService->newBlockUpdateStruct($block);
+
+        foreach ($block->getAllConfigs() as $configIdentifier => $config) {
+            if ($identifier !== null && $configIdentifier !== $identifier) {
+                continue;
+            }
+
+            $configStruct = new ConfigStruct();
+            $configStruct->fillValues($config->getDefinition(), $config->getParameters(), false);
+            $updateStruct->setConfigStruct($configIdentifier, $configStruct);
+        }
+
+        $form = $this->createForm(
+            ConfigEditType::class,
+            $updateStruct,
+            array(
+                'configurable' => $block,
+                'configType' => 'block',
+                'action' => $this->generateUrl(
+                    'ngbm_app_block_form_edit_config',
+                    array(
+                        'blockId' => $block->getId(),
+                        'identifier' => $identifier,
                     )
                 ),
             )
