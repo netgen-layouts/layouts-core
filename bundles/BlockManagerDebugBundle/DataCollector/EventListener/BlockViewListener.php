10310fc17c64a5fd0c401f1a35f5b674e0b1ff8c
Edi Modrić <edi.modric@gmail.com>
2017-03-25 12:31:51 +0100

    Whitelist contexts in profiler instead of blacklisting them

--- bundles/BlockManagerDebugBundle/DataCollector/EventListener/BlockViewListener.php
+++ bundles/BlockManagerDebugBundle/DataCollector/EventListener/BlockViewListener.php
@@ -5,30 +5,31 @@ namespace Netgen\Bundle\BlockManagerDebugBundle\DataCollector\EventListener;
 use Netgen\BlockManager\Event\BlockManagerEvents;
 use Netgen\BlockManager\Event\CollectViewParametersEvent;
 use Netgen\BlockManager\View\View\BlockViewInterface;
-use Netgen\BlockManager\View\ViewInterface;
 use Netgen\Bundle\BlockManagerDebugBundle\DataCollector\BlockManagerDataCollector;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class BlockViewListener implements EventSubscriberInterface
 {
-    const BLACKLISTED_CONTEXTS = array(
-        ViewInterface::CONTEXT_API,
-        ViewInterface::CONTEXT_ADMIN,
-    );
-
     /**
      * @var \Netgen\Bundle\BlockManagerDebugBundle\DataCollector\BlockManagerDataCollector
      */
     protected $dataCollector;
 
     /**
+     * @var string[]
+     */
+    protected $enabledContexts;
+
+    /**
      * Constructor.
      *
      * @param \Netgen\Bundle\BlockManagerDebugBundle\DataCollector\BlockManagerDataCollector $dataCollector
+     * @param string[] $enabledContexts
      */
-    public function __construct(BlockManagerDataCollector $dataCollector)
+    public function __construct(BlockManagerDataCollector $dataCollector, array $enabledContexts = array())
     {
         $this->dataCollector = $dataCollector;
+        $this->enabledContexts = $enabledContexts;
     }
 
     /**
@@ -54,7 +55,7 @@ class BlockViewListener implements EventSubscriberInterface
             return;
         }
 
-        if (in_array($view->getContext(), self::BLACKLISTED_CONTEXTS, true)) {
+        if (!in_array($view->getContext(), $this->enabledContexts, true)) {
             return;
         }
 
