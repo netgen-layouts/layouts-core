7ab128c10e09cd1620a8781cb2e7afda9c1a1cf2
Edi Modrić <edi.modric@gmail.com>
2017-12-12 13:52:33 +0100

    Disable cache clearing if no client is configured

---
+++ bundles/BlockManagerAdminBundle/EventListener/LayoutView/CacheEnabledListener.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Netgen\Bundle\BlockManagerAdminBundle\EventListener\LayoutView;
+
+use Netgen\BlockManager\Event\BlockManagerEvents;
+use Netgen\BlockManager\Event\CollectViewParametersEvent;
+use Netgen\BlockManager\HttpCache\ClientInterface;
+use Netgen\BlockManager\HttpCache\NullClient;
+use Netgen\BlockManager\View\View\LayoutViewInterface;
+use Netgen\BlockManager\View\ViewInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class CacheEnabledListener implements EventSubscriberInterface
+{
+    /**
+     * @var \Netgen\BlockManager\HttpCache\ClientInterface
+     */
+    private $httpCacheClient;
+
+    /**
+     * @var bool
+     */
+    private $cacheEnabled = true;
+
+    public function __construct(ClientInterface $httpCacheClient)
+    {
+        $this->httpCacheClient = $httpCacheClient;
+        $this->cacheEnabled = !$this->httpCacheClient instanceof NullClient;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(BlockManagerEvents::BUILD_VIEW => 'onBuildView');
+    }
+
+    /**
+     * Injects if the HTTP cache clearing is enabled or not.
+     *
+     * @param \Netgen\BlockManager\Event\CollectViewParametersEvent $event
+     */
+    public function onBuildView(CollectViewParametersEvent $event)
+    {
+        $view = $event->getView();
+        if (!$view instanceof LayoutViewInterface) {
+            return;
+        }
+
+        if ($view->getContext() !== ViewInterface::CONTEXT_ADMIN) {
+            return;
+        }
+
+        $event->addParameter('http_cache_enabled', $this->cacheEnabled);
+    }
+}
