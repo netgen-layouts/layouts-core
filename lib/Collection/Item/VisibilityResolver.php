3b9e69c9baeb19cad6150d40dcf23d1d21fd6a4e
Edi Modrić <edi.modric@gmail.com>
2018-03-21 12:16:33 +0100

    Add logging for items declared as hidden by visibility voter

--- lib/Collection/Item/VisibilityResolver.php
+++ lib/Collection/Item/VisibilityResolver.php
@@ -5,13 +5,25 @@ namespace Netgen\BlockManager\Collection\Item;
 use Netgen\BlockManager\API\Values\Collection\Item;
 use Netgen\BlockManager\Collection\Item\VisibilityResolver\VoterInterface;
 use Netgen\BlockManager\Exception\InvalidInterfaceException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 final class VisibilityResolver implements VisibilityResolverInterface
 {
     /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @var \Netgen\BlockManager\Collection\Item\VisibilityResolver\VoterInterface[]
      */
     private $voters = array();
+
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger ?: new NullLogger();
+    }
 
     /**
      * Sets the available voters.
@@ -38,6 +50,18 @@ final class VisibilityResolver implements VisibilityResolverInterface
         foreach ($this->voters as $voter) {
             $result = $voter->vote($item);
             if ($result !== VoterInterface::ABSTAIN) {
+                if ($result === VoterInterface::NO) {
+                    $this->logger->info(
+                        sprintf(
+                            'Collection item #%s (type: %s, value: %s) was declared as hidden by "%s" voter',
+                            $item->getId(),
+                            $item->getValueType(),
+                            $item->getValue(),
+                            get_class($voter)
+                        )
+                    );
+                }
+
                 return (bool) $result;
             }
         }
