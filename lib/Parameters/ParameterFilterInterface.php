05b7470ef73ed99c55f7d6ef8b916178dd39c752
Edi Modrić <edi.modric@gmail.com>
2018-03-24 00:14:31 +0100

    PHPDoc

--- lib/Parameters/ParameterFilterInterface.php
+++ lib/Parameters/ParameterFilterInterface.php
@@ -2,6 +2,11 @@
 
 namespace Netgen\BlockManager\Parameters;
 
+/**
+ * Parameter filters are used before validating the parameter values to perform various
+ * transformations. For example, one filter would rewrite the HTML markup parameter value
+ * to markup without any <script> tags or other insecure "features".
+ */
 interface ParameterFilterInterface
 {
     /**
