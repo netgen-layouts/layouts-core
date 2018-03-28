2388497ce47dc2fbcbf857d85c2ff7f6b5a3c648
Edi Modrić <edi.modric@gmail.com>
2018-03-28 12:56:06 +0200

    Set Symfony-Session-NoAutoCacheControl header for Symfony 4.1 and cacheable views

--- bundles/BlockManagerBundle/EventListener/HttpCache/CacheableViewListener.php
+++ bundles/BlockManagerBundle/EventListener/HttpCache/CacheableViewListener.php
@@ -8,6 +8,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 final class CacheableViewListener implements EventSubscriberInterface
@@ -49,7 +51,7 @@ final class CacheableViewListener implements EventSubscriberInterface
         }
 
         $view = $event->getRequest()->attributes->get('ngbmView');
-        if (!$view instanceof CacheableViewInterface) {
+        if (!$view instanceof CacheableViewInterface || !$view->isCacheable()) {
             return;
         }
 
@@ -62,13 +64,16 @@ final class CacheableViewListener implements EventSubscriberInterface
      */
     private function setUpCachingHeaders(CacheableViewInterface $cacheableView, Response $response)
     {
-        if (!$cacheableView->isCacheable()) {
-            return;
-        }
-
         if (!$response->headers->hasCacheControlDirective('s-maxage')) {
             $sharedMaxAge = (int) $cacheableView->getSharedMaxAge();
             $response->setSharedMaxAge($sharedMaxAge);
+        }
+
+        // In Symfony 4.1+, disables setting private Cache-Control header
+        // in case of an active session
+        // https://github.com/symfony/symfony/pull/26681
+        if (Kernel::VERSION_ID >= 40100) {
+            $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, true);
         }
     }
 }
