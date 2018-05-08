fe0cc8054c1da2011f4ad462a1817bb837cec166
Edi Modrić <edi.modric@gmail.com>
2018-05-08 17:50:10 +0200

    Master is 0.12

--- lib/Version.php
+++ lib/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\BlockManager;
 
 abstract class Version
 {
-    const VERSION = '0.11.0-DEV';
-    const VERSION_ID = 1100;
+    const VERSION = '0.12.0-DEV';
+    const VERSION_ID = 1200;
     const MAJOR_VERSION = 0;
-    const MINOR_VERSION = 11;
+    const MINOR_VERSION = 12;
     const RELEASE_VERSION = 0;
     const EXTRA_VERSION = 'DEV';
 }
