07af865ec75e2023329eea72f423bedfaefcc2cd
Edi Modrić <edi.modric@gmail.com>
2018-05-17 13:49:44 +0200

    Use port 4242 for behat tests

--- tests/run_selenium.sh
+++ tests/run_selenium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-php tests/application/bin/console server:stop
-php tests/application/bin/console server:start 127.0.0.1:8000 -d tests/application/web
+php tests/application/bin/console server:stop --no-debug --env=prod --quiet
+php tests/application/bin/console server:start --no-debug --env=prod 127.0.0.1:4242 -d tests/application/web
 
 if [ ! -f vendor/bin/selenium.jar ]; then
     curl http://selenium-release.storage.googleapis.com/3.12/selenium-server-standalone-3.12.0.jar > vendor/bin/selenium.jar
