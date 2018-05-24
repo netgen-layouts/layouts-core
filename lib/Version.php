3910212c95d43753852d72f886c2438223b1f534
Edi Modrić <edi.modric@gmail.com>
2018-05-24 12:35:11 +0200

    Bump version to 0.11.3

--- lib/Version.php
+++ lib/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\BlockManager;
 
 abstract class Version
 {
-    const VERSION = '0.11.2';
-    const VERSION_ID = 1102;
+    const VERSION = '0.11.3-DEV';
+    const VERSION_ID = 1103;
     const MAJOR_VERSION = 0;
     const MINOR_VERSION = 11;
-    const RELEASE_VERSION = 2;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 3;
+    const EXTRA_VERSION = 'DEV';
 }
