5e1abc0c45ba19da6226a2d836e22f7826a42862
Edi Modrić <edi.modric@gmail.com>
2018-06-12 14:19:41 +0200

    Release 0.11.3

--- lib/Version.php
+++ lib/Version.php
@@ -4,10 +4,10 @@ namespace Netgen\BlockManager;
 
 abstract class Version
 {
-    const VERSION = '0.11.3-DEV';
+    const VERSION = '0.11.3';
     const VERSION_ID = 1103;
     const MAJOR_VERSION = 0;
     const MINOR_VERSION = 11;
     const RELEASE_VERSION = 3;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 }
