53f5ac2487e323662acb72e0e5bf17e63e09475a
Edi Modrić <edi.modric@gmail.com>
2018-06-13 13:05:04 +0200

    Remove PHP 5.6 specific code

--- tests/bundles/BlockManagerBundle/Templating/Twig/Node/NodeTest.php
+++ tests/bundles/BlockManagerBundle/Templating/Twig/Node/NodeTest.php
@@ -17,10 +17,6 @@ abstract class NodeTest extends NodeTestCase
             return sprintf('%s(isset($context["%s"]) || array_key_exists("%s", $context) ? $context["%s"] : (function () { throw new Twig_Error_Runtime(\'Variable "%s" does not exist.\', 1, $this->source); })())', $line, $name, $name, $name, $name);
         }
 
-        if (\PHP_VERSION_ID >= 70000) {
-            return sprintf('%s($context["%s"] ?? $this->getContext($context, "%s"))', $line, $name, $name);
-        }
-
-        return sprintf('%s(isset($context["%s"]) ? $context["%s"] : $this->getContext($context, "%s"))', $line, $name, $name, $name);
+        return sprintf('%s($context["%s"] ?? $this->getContext($context, "%s"))', $line, $name, $name);
     }
 }
