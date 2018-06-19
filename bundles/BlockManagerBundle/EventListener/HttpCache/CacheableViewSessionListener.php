212d0467d71de8cd567f404431d694137fb907ba
Edi Modrić <edi.modric@gmail.com>
2018-06-19 17:06:52 +0200

    Add support for onFinishRequest method in CacheableViewListener

--- bundles/BlockManagerBundle/EventListener/HttpCache/CacheableViewSessionListener.php
+++ bundles/BlockManagerBundle/EventListener/HttpCache/CacheableViewSessionListener.php
@@ -5,6 +5,7 @@ namespace Netgen\Bundle\BlockManagerBundle\EventListener\HttpCache;
 use Netgen\BlockManager\View\CacheableViewInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\EventListener\SessionListener;
 
@@ -36,6 +37,13 @@ final class CacheableViewSessionListener implements EventSubscriberInterface
     public function onKernelRequest(GetResponseEvent $event)
     {
         return $this->innerListener->onKernelRequest($event);
+    }
+
+    public function onFinishRequest(FinishRequestEvent $event)
+    {
+        if (method_exists($this->innerListener, 'onFinishRequest')) {
+            $this->innerListener->onFinishRequest($event);
+        }
     }
 
     public function onKernelResponse(FilterResponseEvent $event)
