5230881fdea80ecd8defb21879aa15881068cea9
Edi Modrić <edi.modric@gmail.com>
2019-04-12 22:37:35 +0200

    Add missing public dir path for prepare_behat.sh script

--- tests/prepare_behat.sh
+++ tests/prepare_behat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 php tests/application/bin/console server:stop --no-debug --env=prod --quiet
-php tests/application/bin/console server:start --no-debug --env=prod 127.0.0.1:4242
+php tests/application/bin/console server:start --no-debug --env=prod 127.0.0.1:4242 -d tests/application/public
 
 if [ ! -f vendor/bin/chromedriver ]; then
     LATEST_CHROMEDRIVER=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
