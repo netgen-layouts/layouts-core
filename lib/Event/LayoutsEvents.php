9ddbbc08e8aec3e00f311e08c2bb51d941823a17
Edi Modrić <edi.modric@gmail.com>
2019-04-12 10:16:41 +0200

    Rename event internal names

--- lib/Event/LayoutsEvents.php
+++ lib/Event/LayoutsEvents.php
@@ -10,17 +10,17 @@ final class LayoutsEvents
      * This event will be dispatched when the view object is being built.
      *
      * In addition to this event, every view has its own event, e.g. for block,
-     * the event is called "ngbm.view.build_view.block" that is being dispatched
+     * the event is called "nglayouts.view.build_view.block" that is being dispatched
      * after the main event.
      */
-    public const BUILD_VIEW = 'ngbm.view.build_view';
+    public const BUILD_VIEW = 'nglayouts.view.build_view';
 
     /**
      * This event will be dispatched when the view object is being rendered.
      *
      * In addition to this event, every view has its own event, e.g. for block,
-     * the event is called "ngbm.view.render_view.block" that is being dispatched
+     * the event is called "nglayouts.view.render_view.block" that is being dispatched
      * after the main event.
      */
-    public const RENDER_VIEW = 'ngbm.view.render_view';
+    public const RENDER_VIEW = 'nglayouts.view.render_view';
 }
