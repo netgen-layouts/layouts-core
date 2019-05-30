cd13f06363a31b0d00d00e8c062925a30afe8939
Edi Modrić <edi.modric@gmail.com>
2019-05-30 20:13:57 +0200

    Bump FE dependencies

--- webpack.config.admin.js
+++ webpack.config.admin.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
