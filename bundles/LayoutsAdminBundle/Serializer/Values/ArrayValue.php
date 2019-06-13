5bddee6b5b3d73438efa50c82945c47ae7411689
Edi Modrić <edi.modric@gmail.com>
2019-06-13 15:31:04 +0200

    Make sure serialized ArrayValue is actually an array

--- bundles/LayoutsAdminBundle/Serializer/Values/ArrayValue.php
+++ bundles/LayoutsAdminBundle/Serializer/Values/ArrayValue.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\LayoutsAdminBundle\Serializer\Values;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Represents a serialized array.
  */
 final class ArrayValue extends AbstractValue
 {
+    public function __construct(array $value, int $statusCode = Response::HTTP_OK)
+    {
+        parent::__construct($value, $statusCode);
+    }
 }
