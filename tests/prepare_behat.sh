cc3cf9120ee94f22a34fd1956101ae362c49e799
Edi Modrić <edi.modric@gmail.com>
2019-06-14 11:25:43 +0200

    Use Chrome Driver 74 for now, as Chrome 75 is supported by Mink

--- tests/prepare_behat.sh
+++ tests/prepare_behat.sh
@@ -4,7 +4,7 @@ php tests/application/bin/console server:stop --no-debug --env=prod --quiet
 php tests/application/bin/console server:start --no-debug --env=prod 127.0.0.1:4242 -d tests/application/public
 
 if [ ! -f vendor/bin/chromedriver ]; then
-    LATEST_CHROMEDRIVER=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+    LATEST_CHROMEDRIVER=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE_74)
     curl http://chromedriver.storage.googleapis.com/$LATEST_CHROMEDRIVER/chromedriver_linux64.zip > chromedriver.zip
     unzip chromedriver.zip && rm chromedriver.zip
     mv -f chromedriver vendor/bin/
