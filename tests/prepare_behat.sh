0124336088859cbf09fdb4a62715b32522d8b3d1
Edi Modrić <edi.modric@gmail.com>
2019-07-15 15:00:20 +0200

    Switch behat tests to Symfony CLI

--- tests/prepare_behat.sh
+++ tests/prepare_behat.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-php tests/application/bin/console server:stop --no-debug --env=prod --quiet
-php tests/application/bin/console server:start --no-debug --env=prod 127.0.0.1:4242 -d tests/application/public
+if [ ! -f vendor/bin/symfony ]; then
+    LATEST_SYMFONY_CLI=$(wget -qO- https://get.symfony.com/cli/LATEST)
+    curl -L https://github.com/symfony/cli/releases/download/v$LATEST_SYMFONY_CLI/symfony_linux_amd64.gz > symfony.gz
+    gunzip symfony.gz && chmod 755 symfony
+    mv -f symfony vendor/bin/
+fi
 
 if [ ! -f vendor/bin/chromedriver ]; then
     # MAJOR_CHROME_VERSION=$(google-chrome --product-version | grep -o '^[0-9]\+')
@@ -12,4 +16,6 @@ if [ ! -f vendor/bin/chromedriver ]; then
     mv -f chromedriver vendor/bin/
 fi
 
+vendor/bin/symfony server:ca:install
+vendor/bin/symfony server:start --daemon --port=4242 --document-root=tests/application/public
 vendor/bin/chromedriver
