acd0fb50c73180d0ba40ba0cea54e692aed2cefd
Edi Modrić <edi.modric@gmail.com>
2019-12-11 10:29:43 +0100

    Add AJAX endpoint for displaying layouts related to a shared layout

---
+++ bundles/LayoutsAdminBundle/Controller/Admin/SharedLayouts/RelatedLayouts.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\LayoutsAdminBundle\Controller\Admin\SharedLayouts;
+
+use Netgen\Bundle\LayoutsBundle\Controller\AbstractController;
+use Netgen\Layouts\API\Service\LayoutService;
+use Netgen\Layouts\API\Values\Layout\Layout;
+use Symfony\Component\HttpFoundation\Request;
+
+final class RelatedLayouts extends AbstractController
+{
+    /**
+     * @var \Netgen\Layouts\API\Service\LayoutService
+     */
+    private $layoutService;
+
+    public function __construct(LayoutService $layoutService)
+    {
+        $this->layoutService = $layoutService;
+    }
+
+    /**
+     * Loads and displays all layouts related to a provided layout.
+     *
+     * @return \Netgen\Layouts\View\ViewInterface|\Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(Layout $layout, Request $request)
+    {
+        $this->denyAccessUnlessGranted('nglayouts:ui:access');
+
+        return $this->render(
+            '@NetgenLayoutsAdmin/admin/shared_layouts/related_layouts.html.twig',
+            [
+                'layout' => $layout,
+                'related_layouts' => $this->layoutService->loadRelatedLayouts($layout),
+            ]
+        );
+    }
+}
