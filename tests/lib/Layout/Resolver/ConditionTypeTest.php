3b5fcafa84c5f1652f8e3a6369f6f406fe14b768
Edi Modrić <edi.modric@gmail.com>
2020-07-01 14:58:03 +0200

    Add tests for abstract TargetType and ConditionType

---
+++ tests/lib/Layout/Resolver/ConditionTypeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Tests\Layout\Resolver;
+
+use Netgen\Layouts\Tests\Layout\Resolver\Stubs\ConditionType2;
+use PHPUnit\Framework\TestCase;
+
+final class ConditionTypeTest extends TestCase
+{
+    /**
+     * @var \Netgen\Layouts\Layout\Resolver\ConditionTypeInterface
+     */
+    private $conditionType;
+
+    protected function setUp(): void
+    {
+        $this->conditionType = new ConditionType2();
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Layout\Resolver\ConditionType::export
+     */
+    public function testExport(): void
+    {
+        self::assertSame('foo', $this->conditionType->export('foo'));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Layout\Resolver\ConditionType::import
+     */
+    public function testImport(): void
+    {
+        self::assertSame('foo', $this->conditionType->import('foo'));
+    }
+}
---
+++ tests/lib/Layout/Resolver/TargetTypeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Tests\Layout\Resolver;
+
+use Netgen\Layouts\Tests\Layout\Resolver\Stubs\TargetType1;
+use PHPUnit\Framework\TestCase;
+
+final class TargetTypeTest extends TestCase
+{
+    /**
+     * @var \Netgen\Layouts\Layout\Resolver\TargetTypeInterface
+     */
+    private $targetType;
+
+    protected function setUp(): void
+    {
+        $this->targetType = new TargetType1();
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Layout\Resolver\TargetType::export
+     */
+    public function testExport(): void
+    {
+        self::assertSame('foo', $this->targetType->export('foo'));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Layout\Resolver\TargetType::import
+     */
+    public function testImport(): void
+    {
+        self::assertSame('foo', $this->targetType->import('foo'));
+    }
+}
