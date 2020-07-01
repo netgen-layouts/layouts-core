9608c227211f6077b9c895ed67e4481967083bed
Edi Modrić <edi.modric@gmail.com>
2020-07-01 13:49:07 +0200

    Add tests for transfer entity handlers

---
+++ tests/lib/Transfer/EntityHandler/LayoutEntityHandlerTest.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Tests\Transfer\EntityHandler;
+
+use Netgen\Layouts\API\Service\BlockService;
+use Netgen\Layouts\API\Service\CollectionService;
+use Netgen\Layouts\API\Service\LayoutService;
+use Netgen\Layouts\API\Values\Layout\Layout;
+use Netgen\Layouts\Block\Registry\BlockDefinitionRegistry;
+use Netgen\Layouts\Collection\Registry\ItemDefinitionRegistry;
+use Netgen\Layouts\Collection\Registry\QueryTypeRegistry;
+use Netgen\Layouts\Exception\NotFoundException;
+use Netgen\Layouts\Item\CmsItemLoaderInterface;
+use Netgen\Layouts\Layout\Registry\LayoutTypeRegistry;
+use Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use function sprintf;
+
+final class LayoutEntityHandlerTest extends TestCase
+{
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $layoutServiceMock;
+
+    /**
+     * @var \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler
+     */
+    private $entityHandler;
+
+    protected function setUp(): void
+    {
+        $this->layoutServiceMock = $this->createMock(LayoutService::class);
+
+        $this->entityHandler = new LayoutEntityHandler(
+            $this->createMock(BlockService::class),
+            $this->createMock(CollectionService::class),
+            $this->layoutServiceMock,
+            new BlockDefinitionRegistry([]),
+            new LayoutTypeRegistry([]),
+            new ItemDefinitionRegistry([]),
+            new QueryTypeRegistry([]),
+            $this->createMock(CmsItemLoaderInterface::class)
+        );
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::__construct
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::loadEntity
+     */
+    public function testLoadEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $layout = Layout::fromArray(['id' => $uuid]);
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method('loadLayout')
+            ->with(self::identicalTo($uuid))
+            ->willReturn($layout);
+
+        self::assertSame($layout, $this->entityHandler->loadEntity($uuid));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::loadEntity
+     */
+    public function testLoadEntityWithNonExistentEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->expectException(NotFoundException::class);
+        $this->expectExceptionMessage(sprintf('Could not find layout with identifier "%s"', $uuid->toString()));
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method('loadLayout')
+            ->with(self::identicalTo($uuid))
+            ->willThrowException(new NotFoundException('layout', $uuid->toString()));
+
+        $this->entityHandler->loadEntity($uuid);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::entityExists
+     */
+    public function testEntityExists(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method('layoutExists')
+            ->with(self::identicalTo($uuid))
+            ->willReturn(true);
+
+        self::assertTrue($this->entityHandler->entityExists($uuid));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::entityExists
+     */
+    public function testEntityExistsReturnsFalse(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method('layoutExists')
+            ->with(self::identicalTo($uuid))
+            ->willReturn(false);
+
+        self::assertFalse($this->entityHandler->entityExists($uuid));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::deleteEntity
+     */
+    public function testDeleteEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $layout = Layout::fromArray(['id' => $uuid]);
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method('loadLayout')
+            ->with(self::identicalTo($uuid))
+            ->willReturn($layout);
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method(('deleteLayout'))
+            ->with(self::identicalTo($layout));
+
+        $this->entityHandler->deleteEntity($uuid);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\LayoutEntityHandler::deleteEntity
+     */
+    public function testDeleteEntityWithNonExistentEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->expectException(NotFoundException::class);
+        $this->expectExceptionMessage(sprintf('Could not find layout with identifier "%s"', $uuid->toString()));
+
+        $this->layoutServiceMock
+            ->expects(self::once())
+            ->method('loadLayout')
+            ->with(self::identicalTo($uuid))
+            ->willThrowException(new NotFoundException('layout', $uuid->toString()));
+
+        $this->layoutServiceMock
+            ->expects(self::never())
+            ->method(('deleteLayout'));
+
+        $this->entityHandler->deleteEntity($uuid);
+    }
+}
---
+++ tests/lib/Transfer/EntityHandler/RuleEntityHandlerTest.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Tests\Transfer\EntityHandler;
+
+use Netgen\Layouts\API\Service\LayoutResolverService;
+use Netgen\Layouts\API\Values\LayoutResolver\Rule;
+use Netgen\Layouts\Exception\NotFoundException;
+use Netgen\Layouts\Layout\Resolver\Registry\ConditionTypeRegistry;
+use Netgen\Layouts\Layout\Resolver\Registry\TargetTypeRegistry;
+use Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use function sprintf;
+
+final class RuleEntityHandlerTest extends TestCase
+{
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $layoutResolverServiceMock;
+
+    /**
+     * @var \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler
+     */
+    private $entityHandler;
+
+    protected function setUp(): void
+    {
+        $this->layoutResolverServiceMock = $this->createMock(LayoutResolverService::class);
+
+        $this->entityHandler = new RuleEntityHandler(
+            $this->layoutResolverServiceMock,
+            new TargetTypeRegistry([]),
+            new ConditionTypeRegistry([])
+        );
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::__construct
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::loadEntity
+     */
+    public function testLoadEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $rule = Rule::fromArray(['id' => $uuid]);
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method('loadRule')
+            ->with(self::identicalTo($uuid))
+            ->willReturn($rule);
+
+        self::assertSame($rule, $this->entityHandler->loadEntity($uuid));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::loadEntity
+     */
+    public function testLoadEntityWithNonExistentEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->expectException(NotFoundException::class);
+        $this->expectExceptionMessage(sprintf('Could not find rule with identifier "%s"', $uuid->toString()));
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method('loadRule')
+            ->with(self::identicalTo($uuid))
+            ->willThrowException(new NotFoundException('rule', $uuid->toString()));
+
+        $this->entityHandler->loadEntity($uuid);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::entityExists
+     */
+    public function testEntityExists(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method('ruleExists')
+            ->with(self::identicalTo($uuid))
+            ->willReturn(true);
+
+        self::assertTrue($this->entityHandler->entityExists($uuid));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::entityExists
+     */
+    public function testEntityExistsReturnsFalse(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method('ruleExists')
+            ->with(self::identicalTo($uuid))
+            ->willReturn(false);
+
+        self::assertFalse($this->entityHandler->entityExists($uuid));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::deleteEntity
+     */
+    public function testDeleteEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $rule = Rule::fromArray(['id' => $uuid]);
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method('loadRule')
+            ->with(self::identicalTo($uuid))
+            ->willReturn($rule);
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method(('deleteRule'))
+            ->with(self::identicalTo($rule));
+
+        $this->entityHandler->deleteEntity($uuid);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Transfer\EntityHandler\RuleEntityHandler::deleteEntity
+     */
+    public function testDeleteEntityWithNonExistentEntity(): void
+    {
+        $uuid = Uuid::uuid4();
+
+        $this->expectException(NotFoundException::class);
+        $this->expectExceptionMessage(sprintf('Could not find rule with identifier "%s"', $uuid->toString()));
+
+        $this->layoutResolverServiceMock
+            ->expects(self::once())
+            ->method('loadRule')
+            ->with(self::identicalTo($uuid))
+            ->willThrowException(new NotFoundException('rule', $uuid->toString()));
+
+        $this->layoutResolverServiceMock
+            ->expects(self::never())
+            ->method(('deleteRule'));
+
+        $this->entityHandler->deleteEntity($uuid);
+    }
+}
