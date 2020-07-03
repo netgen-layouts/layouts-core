6305698cbfe0da2b17c98b945ad05d22e9ba8d58
Edi Modrić <edi.modric@gmail.com>
2020-07-03 10:11:10 +0200

    Enhance a bit error display in case of schema validation failures

--- lib/Transfer/Input/JsonValidator.php
+++ lib/Transfer/Input/JsonValidator.php
@@ -7,9 +7,13 @@ namespace Netgen\Layouts\Transfer\Input;
 use JsonException;
 use Netgen\Layouts\Exception\Transfer\JsonValidationException;
 use stdClass;
+use Swaggest\JsonSchema\Exception\LogicException;
 use Swaggest\JsonSchema\Schema;
 use Throwable;
+use function array_key_last;
+use function count;
 use function get_debug_type;
+use function is_array;
 use function json_decode;
 use function sprintf;
 use const JSON_THROW_ON_ERROR;
@@ -23,6 +27,14 @@ final class JsonValidator implements JsonValidatorInterface
 
         try {
             Schema::import($parsedSchema)->in($parsedData);
+        } catch (LogicException $e) {
+            $message = $e->getMessage();
+
+            if (is_array($e->subErrors) && count($e->subErrors) > 0) {
+                $message = $e->subErrors[array_key_last($e->subErrors)]->error;
+            }
+
+            throw JsonValidationException::validationFailed($message, $e);
         } catch (Throwable $t) {
             throw JsonValidationException::validationFailed($t->getMessage(), $t);
         }
