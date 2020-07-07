ff32376499e7ddc60f75dd6447b884a5e89e3770
Ljudevit Juras <ljudevit@cjdizajn.hr>
2020-07-07 10:32:31 +0200

    Add more specific condition for filename display container

--- bundles/LayoutsAdminBundle/Resources/es6/components/forms.js
+++ bundles/LayoutsAdminBundle/Resources/es6/components/forms.js
@@ -3,8 +3,8 @@ const formsInit = () => {
     const fileInputs = document.querySelectorAll('input[type=file]');
     [...fileInputs].forEach((i) => {
         i.addEventListener('change', () => {
-          const result = i.previousSibling; // catching span to display file name
-          result.textContent = `${i.files[0].name}`;
+            const result = i.parentNode.getElementsByClassName('filename')[0]; // catching span to display file name
+            result.textContent = `${i.files[0].name}`;
         });
     });
 };
