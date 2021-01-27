e94c0de5b76e89f902fb3abbef5db3a37f519002
Edi Modrić <edi.modric@gmail.com>
2021-01-27 16:11:33 +0100

    Update v1.3 migrations to include docoupling of conditions

--- migrations/Doctrine/Version010300.php
+++ migrations/Doctrine/Version010300.php
@@ -46,9 +46,9 @@ final class Version010300 extends AbstractMigration
         $this->addSql(
             <<<'EOT'
             INSERT INTO nglayouts_rule_group (
-                `id`, `status`, `uuid`, `depth`, `path`, `parent_id`, `comment`
+              `id`, `status`, `uuid`, `depth`, `path`, `parent_id`, `comment`
             ) VALUES (
-                1, 1, '00000000-0000-0000-0000-000000000000', 0, '/1/', NULL, ''
+              1, 1, '00000000-0000-0000-0000-000000000000', 0, '/1/', NULL, ''
             )
             EOT
         );
@@ -56,19 +56,80 @@ final class Version010300 extends AbstractMigration
         $this->addSql(
             <<<'EOT'
             INSERT INTO nglayouts_rule_group_data (
-                `rule_group_id`, `enabled`, `priority`
+              `rule_group_id`, `enabled`, `priority`
             ) VALUES (
-                1, 1, 0
+              1, 1, 0
             )
             EOT
         );
 
         $this->addSql('UPDATE nglayouts_rule SET rule_group_id = 1');
+
+        $this->addSql(
+            <<<'EOT'
+            CREATE TABLE `nglayouts_rule_condition_rule` (
+              `condition_id` int NOT NULL,
+              `condition_status` int NOT NULL,
+              `rule_id` int NOT NULL,
+              `rule_status` int NOT NULL,
+              PRIMARY KEY (`condition_id`,`condition_status`),
+              KEY `idx_ngl_rule` (`rule_id`,`rule_status`),
+              CONSTRAINT `fk_ngl_rule_condition_rule_rule_condition` FOREIGN KEY (`condition_id`, `condition_status`) REFERENCES `nglayouts_rule_condition` (`id`, `status`),
+              CONSTRAINT `fk_ngl_rule_condition_rule_rule` FOREIGN KEY (`rule_id`, `rule_status`) REFERENCES `nglayouts_rule` (`id`, `status`)
+            )
+            EOT
+        );
+
+        $this->addSql(
+            <<<'EOT'
+            CREATE TABLE `nglayouts_rule_condition_rule_group` (
+              `condition_id` int NOT NULL,
+              `condition_status` int NOT NULL,
+              `rule_group_id` int NOT NULL,
+              `rule_group_status` int NOT NULL,
+              PRIMARY KEY (`condition_id`,`condition_status`),
+              KEY `idx_ngl_rule_group` (`rule_group_id`,`rule_group_status`),
+              CONSTRAINT `fk_ngl_rule_condition_rule_group_rule_condition` FOREIGN KEY (`condition_id`, `condition_status`) REFERENCES `nglayouts_rule_condition` (`id`, `status`),
+              CONSTRAINT `fk_ngl_rule_condition_rule_group_rule_group` FOREIGN KEY (`rule_group_id`, `rule_group_status`) REFERENCES `nglayouts_rule_group` (`id`, `status`)
+            )
+            EOT
+        );
+
+        $this->addSql(
+            <<<'EOT'
+            INSERT INTO `nglayouts_rule_condition_rule` (
+              `condition_id`,
+              `condition_status`,
+              `rule_id`,
+              `rule_status`
+            ) SELECT `id`, `status`, `rule_id`, `status` FROM `nglayouts_rule_condition`
+            EOT
+        );
+
+        $this->addSql('ALTER TABLE `nglayouts_rule_condition` DROP FOREIGN KEY `fk_ngl_condition_rule`');
+        $this->addSql('ALTER TABLE `nglayouts_rule_condition` DROP COLUMN `rule_id`');
     }
 
     public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on MySQL.');
+
+        $this->addSql('ALTER TABLE `nglayouts_rule_condition` ADD COLUMN `rule_id` int(11) NOT NULL AFTER `uuid`');
+
+        $this->addSql(
+            <<<'EOT'
+            UPDATE `nglayouts_rule_condition`
+            INNER JOIN `nglayouts_rule_condition_rule`
+                ON `nglayouts_rule_condition`.`id` = `nglayouts_rule_condition_rule`.`condition_id`
+                AND `nglayouts_rule_condition`.`status` = `nglayouts_rule_condition_rule`.`condition_status`
+            SET `nglayouts_rule_condition`.`rule_id` = `nglayouts_rule_condition_rule`.`rule_id`
+            EOT
+        );
+
+        $this->addSql('ALTER TABLE `nglayouts_rule_condition` ADD CONSTRAINT `fk_ngl_condition_rule` FOREIGN KEY (`rule_id`, `status`) REFERENCES `nglayouts_rule` (`id`, `status`)');
+
+        $this->addSql('DROP TABLE nglayouts_rule_condition_rule_group');
+        $this->addSql('DROP TABLE nglayouts_rule_condition_rule');
 
         $this->addSql('ALTER TABLE nglayouts_rule DROP COLUMN rule_group_id');
         $this->addSql('DROP TABLE nglayouts_rule_group_data');
