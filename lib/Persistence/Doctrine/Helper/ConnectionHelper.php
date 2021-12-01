f8817eb5a5a780bfe09fea3a986e44e14a72b28b
Edi Modrić <edi.modric@gmail.com>
2021-12-01 10:16:43 +0100

    Do not use deprecated DBAL AbstractPlatform::getName()

--- lib/Persistence/Doctrine/Helper/ConnectionHelper.php
+++ lib/Persistence/Doctrine/Helper/ConnectionHelper.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace Netgen\Layouts\Persistence\Doctrine\Helper;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper\Postgres;
 use Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper\Sqlite;
+use function is_a;
 
 final class ConnectionHelper implements ConnectionHelperInterface
 {
     private Connection $connection;
 
     /**
-     * @var array<string, \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelperInterface>
+     * @var array<class-string, \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelperInterface>
      */
     private array $databaseSpecificHelpers;
 
@@ -22,17 +26,17 @@ final class ConnectionHelper implements ConnectionHelperInterface
         $this->connection = $connection;
 
         $this->databaseSpecificHelpers = [
-            'sqlite' => new Sqlite($this->connection),
-            'postgresql' => new Postgres($this->connection),
+            SqlitePlatform::class => new Sqlite($this->connection),
+            PostgreSQLPlatform::class => new Postgres($this->connection),
         ];
     }
 
     public function nextId(string $table, string $column = 'id')
     {
-        $databaseServer = $this->connection->getDatabasePlatform()->getName();
-        if (isset($this->databaseSpecificHelpers[$databaseServer])) {
-            return $this->databaseSpecificHelpers[$databaseServer]
-                ->nextId($table, $column);
+        $handler = $this->getHandler($this->connection->getDatabasePlatform());
+
+        if ($handler !== null) {
+            return $handler->nextId($table, $column);
         }
 
         return 'null';
@@ -40,12 +44,23 @@ final class ConnectionHelper implements ConnectionHelperInterface
 
     public function lastId(string $table, string $column = 'id')
     {
-        $databaseServer = $this->connection->getDatabasePlatform()->getName();
-        if (isset($this->databaseSpecificHelpers[$databaseServer])) {
-            return $this->databaseSpecificHelpers[$databaseServer]
-                ->lastId($table, $column);
+        $handler = $this->getHandler($this->connection->getDatabasePlatform());
+
+        if ($handler !== null) {
+            return $handler->lastId($table, $column);
         }
 
         return $this->connection->lastInsertId($table);
+    }
+
+    private function getHandler(AbstractPlatform $platform): ?ConnectionHelperInterface
+    {
+        foreach ($this->databaseSpecificHelpers as $class => $helper) {
+            if (is_a($platform, $class)) {
+                return $helper;
+            }
+        }
+
+        return null;
     }
 }
--- tests/lib/Persistence/Doctrine/Helper/ConnectionHelperTest.php
+++ tests/lib/Persistence/Doctrine/Helper/ConnectionHelperTest.php
@@ -5,111 +5,105 @@ declare(strict_types=1);
 namespace Netgen\Layouts\Tests\Persistence\Doctrine\Helper;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use function class_exists;
+
+// Deprecated DBAL 2 compatibility
+class_exists('Doctrine\DBAL\Platforms\MySqlPlatform');
+class_exists('Doctrine\DBAL\Platforms\PostgreSqlPlatform');
 
 final class ConnectionHelperTest extends TestCase
 {
-    private MockObject $databasePlatformMock;
-
-    private MockObject $databaseConnectionMock;
-
-    private ConnectionHelper $connectionHelper;
-
-    protected function setUp(): void
-    {
-        $this->databasePlatformMock = $this->createMock(AbstractPlatform::class);
-        $this->databaseConnectionMock = $this->createMock(Connection::class);
-
-        $this->databaseConnectionMock
-            ->expects(self::any())
-            ->method('getDatabasePlatform')
-            ->willReturn($this->databasePlatformMock);
-
-        $this->connectionHelper = new ConnectionHelper($this->databaseConnectionMock);
-    }
-
     /**
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::__construct
+     * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::getHandler
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::nextId
      */
     public function testNextId(): void
     {
-        $this->databasePlatformMock
-            ->expects(self::any())
-            ->method('getName')
-            ->willReturn('mysql');
+        $platform = new MySQLPlatform();
+        $connectionMock = $this->createMock(Connection::class);
+        $helper = new ConnectionHelper($connectionMock);
 
-        self::assertSame('null', $this->connectionHelper->nextId('table'));
+        $connectionMock
+            ->expects(self::any())
+            ->method('getDatabasePlatform')
+            ->willReturn($platform);
+
+        self::assertSame('null', $helper->nextId('table'));
     }
 
     /**
+     * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::getHandler
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::nextId
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper\Postgres::__construct
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper\Postgres::nextId
      */
     public function testNextIdForPostgres(): void
     {
-        $this->databasePlatformMock
+        $platform = new PostgreSQLPlatform();
+        $connectionMock = $this->createMock(Connection::class);
+        $helper = new ConnectionHelper($connectionMock);
+
+        $connectionMock
             ->expects(self::any())
-            ->method('getName')
-            ->willReturn('postgresql');
+            ->method('getDatabasePlatform')
+            ->willReturn($platform);
 
-        $this->databasePlatformMock
-            ->expects(self::once())
-            ->method('getIdentitySequenceName')
-            ->with(self::identicalTo('table'), self::identicalTo('id'))
-            ->willReturn('s_table_id');
-
-        self::assertSame("nextval('s_table_id')", $this->connectionHelper->nextId('table'));
+        self::assertSame("nextval('table_id_seq')", $helper->nextId('table'));
     }
 
     /**
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::__construct
+     * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::getHandler
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::lastId
      */
     public function testLastId(): void
     {
-        $this->databasePlatformMock
-            ->expects(self::any())
-            ->method('getName')
-            ->willReturn('mysql');
+        $platform = new MySQLPlatform();
+        $connectionMock = $this->createMock(Connection::class);
+        $helper = new ConnectionHelper($connectionMock);
 
-        $this->databaseConnectionMock
+        $connectionMock
+            ->expects(self::any())
+            ->method('getDatabasePlatform')
+            ->willReturn($platform);
+
+        $connectionMock
             ->expects(self::any())
             ->method('lastInsertId')
             ->with(self::identicalTo('table'))
             ->willReturn(42);
 
-        self::assertSame(42, $this->connectionHelper->lastId('table'));
+        self::assertSame(42, $helper->lastId('table'));
     }
 
     /**
+     * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::getHandler
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper::lastId
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper\Postgres::__construct
      * @covers \Netgen\Layouts\Persistence\Doctrine\Helper\ConnectionHelper\Postgres::lastId
      */
     public function testLastIdForPostgres(): void
     {
-        $this->databasePlatformMock
+        $platform = new PostgreSQLPlatform();
+        $connectionMock = $this->createMock(Connection::class);
+        $helper = new ConnectionHelper($connectionMock);
+
+        $connectionMock
             ->expects(self::any())
-            ->method('getName')
-            ->willReturn('postgresql');
+            ->method('getDatabasePlatform')
+            ->willReturn($platform);
 
-        $this->databasePlatformMock
-            ->expects(self::once())
-            ->method('getIdentitySequenceName')
-            ->with(self::identicalTo('table'), self::identicalTo('id'))
-            ->willReturn('s_table_id');
-
-        $this->databaseConnectionMock
+        $connectionMock
             ->expects(self::any())
             ->method('lastInsertId')
-            ->with(self::identicalTo('s_table_id'))
+            ->with(self::identicalTo('table_id_seq'))
             ->willReturn('43');
 
-        self::assertSame('43', $this->connectionHelper->lastId('table'));
+        self::assertSame('43', $helper->lastId('table'));
     }
 }
