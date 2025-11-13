d882107d62f015570cf4e69ee7a11ef3eb189ff4
Edi Modrić <edi.modric@gmail.com>
2025-11-13 08:55:38 +0100

    Use Count constraint instead of NotIdenticalTo

--- lib/Parameters/ParameterType/ChoiceType.php
+++ lib/Parameters/ParameterType/ChoiceType.php
@@ -85,7 +85,7 @@ final class ChoiceType extends ParameterType
         if ($parameterDefinition->isRequired()) {
             return [
                 new Constraints\NotNull(),
-                new Constraints\NotIdenticalTo(['value' => []]),
+                new Constraints\Count(min: 1),
             ];
         }
 
