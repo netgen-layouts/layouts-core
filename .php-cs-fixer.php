c69c99cc887c316d56a960c72437ac35fe20b5ed
Edi Modrić <edi.modric@gmail.com>
2025-11-14 15:03:28 +0100

    Remove obsolete PHP CS Fixer rule

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,12 +4,6 @@
 require_once __DIR__ . '/vendor/netgen/layouts-coding-standard/lib/PhpCsFixer/Config.php';
 
 return new Netgen\Layouts\CodingStandard\PhpCsFixer\Config()
-    ->addRules([
-        // Makes sure "time" function is not imported to make time sensitive
-        // tests working by overriding the function, which would not be possible
-        // if function was imported from global namespace
-        'native_function_invocation' => ['include' => ['@all'], 'exclude' => ['time']],
-    ])
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->exclude(['vendor', 'node_modules', 'tests/application'])
