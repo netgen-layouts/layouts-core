c0d57a89915f9079d74acf3827f1b2346ef18702
Edi Modrić <edi.modric@gmail.com>
2025-11-27 09:19:08 +0100

    Add possibility to implement lazy objects in hydrator

--- lib/Utils/HydratorTrait.php
+++ lib/Utils/HydratorTrait.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Netgen\Layouts\Utils;
 
+use ReflectionClass;
+
+use function array_map;
+use function count;
+
 trait HydratorTrait
 {
     private static Hydrator $__hydrator;
@@ -13,12 +18,35 @@ trait HydratorTrait
      * and return the object hydrated with provided data.
      *
      * @param array<string, mixed> $data
+     * @param array<string, callable> $lazyInitializers
      */
-    public static function fromArray(array $data): static
+    public static function fromArray(array $data, array $lazyInitializers = []): static
     {
         self::initHydrator();
 
-        return self::$__hydrator->hydrate($data, new static());
+        if (count($lazyInitializers) === 0) {
+            return self::$__hydrator->hydrate($data, new static());
+        }
+
+        $reflector = new ReflectionClass(static::class);
+
+        /** @var static $object */
+        $object = $reflector->newLazyGhost(
+            static function (object $object) use ($lazyInitializers): void {
+                $lazyData = array_map(
+                    static fn (callable $initializer) => $initializer($object),
+                    $lazyInitializers,
+                );
+
+                self::$__hydrator->hydrate($lazyData, $object);
+            },
+        );
+
+        foreach ($data as $property => $value) {
+            $reflector->getProperty($property)->setRawValueWithoutLazyInitialization($object, $value);
+        }
+
+        return $object;
     }
 
     /**
