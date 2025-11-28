9d0097692fb0ec8ac4dc40cc029ddf5f77af2d14
Edi Modrić <edi.modric@gmail.com>
2025-11-28 20:24:50 +0100

    Make method final

--- tests/bundles/LayoutsAdminBundle/Controller/API/ApiTestCase.php
+++ tests/bundles/LayoutsAdminBundle/Controller/API/ApiTestCase.php
@@ -20,7 +20,7 @@ abstract class ApiTestCase extends KernelTestCase
      * @param array<string, mixed> $options
      * @param array<string, mixed> $server
      */
-    protected function browser(array $options = [], array $server = []): KernelBrowser
+    final protected function browser(array $options = [], array $server = []): KernelBrowser
     {
         return $this->baseBrowser($options, $server)
             ->actingAs(new InMemoryUser('admin', 'admin', ['ROLE_NGLAYOUTS_ADMIN']))
