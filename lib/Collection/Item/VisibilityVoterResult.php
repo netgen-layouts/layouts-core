12dab770d8901ad7a9255d325be91c5f18cb36fb
Edi Modrić <edi.modric@gmail.com>
2025-12-15 23:46:03 +0100

    Switch VisibilityVoterResult enum to non-backed enum

--- lib/Collection/Item/VisibilityVoterResult.php
+++ lib/Collection/Item/VisibilityVoterResult.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 namespace Netgen\Layouts\Collection\Item;
 
-enum VisibilityVoterResult: int
+enum VisibilityVoterResult
 {
     /**
      * Returned by the voter if the item is not visible.
      */
-    case No = 0;
+    case No;
 
     /**
      * Returned by the voter if the item is visible.
      */
-    case Yes = 1;
+    case Yes;
 
     /**
      * Returned by the voter if it cannot decide if the item is visible or not.
      */
-    case Abstain = 2;
+    case Abstain;
 }
