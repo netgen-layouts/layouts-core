04e3b659fac9c26549d0715e0c8bd5516015d24e
Edi Modrić <edi.modric@gmail.com>
2025-12-17 15:52:24 +0100

    Exclude tests/application/config from PHP CS Fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/vendor/netgen/layouts-coding-standard/lib/PhpCsFixer/Co
 return new Netgen\Layouts\CodingStandard\PhpCsFixer\Config()
     ->setFinder(
         PhpCsFixer\Finder::create()
-            ->exclude(['vendor', 'node_modules', 'tests/application/public'])
+            ->exclude(['vendor', 'node_modules', 'tests/application/config', 'tests/application/public'])
             ->in(__DIR__)
     )
 ;
